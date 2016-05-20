9ac839a44bad9124620bbbec90d9a5237e862f27
Joseph Birr-Pixton <jpixton@gmail.com>
2016-05-20 22:03:05 +0100

    add dodgy ca script

---
+++ test/build-a-pki.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -xe
+
+openssl req -nodes -x509 -newkey rsa:2048 -keyout ca.key -out ca.cert -sha256 -batch -subj "/CN=ponytown CA"
+openssl req -nodes -newkey rsa:3072 -keyout inter.key -out inter.req -sha256 -batch -subj "/CN=ponytown level 2 intermediate"
+openssl req -nodes -newkey rsa:2048 -keyout end.key -out end.req -sha256 -batch -subj "/CN=testserver.com"
+openssl x509 -req -in inter.req -out inter.cert -CA ca.cert -CAkey ca.key -sha256 -set_serial 123 -extensions v3_inter -extfile openssl.cnf
+openssl x509 -req -in end.req -out end.cert -CA inter.cert -CAkey inter.key -sha256 -set_serial 456 -extensions v3_end -extfile openssl.cnf
