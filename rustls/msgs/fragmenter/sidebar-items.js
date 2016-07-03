f54e63297b5801d9b7ffb6df25c2da74f5d9c086
Joseph Birr-Pixton <jpixton@gmail.com>
2016-07-03 12:42:17 +0100

    Update docs

--- rustls/msgs/fragmenter/sidebar-items.js
+++ rustls/msgs/fragmenter/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["MAX_FRAGMENT_LEN",""]],"struct":[["MessageFragmenter",""]]});
+initSidebarItems({"constant":[["MAX_FRAGMENT_LEN",""],["PACKET_OVERHEAD",""]],"struct":[["MessageFragmenter",""]]});
