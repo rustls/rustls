7535f49a760eada406ddd378bc2f58b9b85aefc7
Joseph Birr-Pixton <jpixton@gmail.com>
2016-09-03 13:13:57 +0100

    Make TLSError implement std::error::Error
    Fixes issue #20

--- src/error.rs
+++ src/error.rs
@@ -1,8 +1,10 @@
+use std::{fmt, error};
+use std::error::Error;
 use msgs::enums::{ContentType, HandshakeType, AlertDescription};
 extern crate webpki;
 
 /// rustls reports protocol errors using this type.
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum TLSError {
   /// We received a TLS message that isn't valid right now.
   /// `expect_types` lists the message types we can expect right now.
@@ -44,4 +46,95 @@ pub enum TLSError {
 
   /// A catch-all error for unlikely errors.
   General(String)
+}
+
+fn join<T: fmt::Debug>(vec: &Vec<T>) -> String {
+  vec
+    .iter()
+    .map(|x| format!("{:?}", x))
+    .collect::<Vec<String>>()
+    .join(" or ")
+}
+
+impl fmt::Display for TLSError {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    match *self {
+      TLSError::InappropriateMessage { ref expect_types, ref got_type }
+        => write!(f, "{}: got {:?} when expecting {}",
+                  self.description(), got_type,
+                  join::<ContentType>(&expect_types)),
+      TLSError::InappropriateHandshakeMessage { ref expect_types, ref got_type }
+        => write!(f, "{}: got {:?} when expecting {}",
+                  self.description(), got_type,
+                  join::<HandshakeType>(&expect_types)),
+      TLSError::CorruptMessagePayload(ref typ)
+        => write!(f, "{} of type {:?}", self.description(), typ),
+      TLSError::PeerIncompatibleError(ref why) | TLSError::PeerMisbehavedError(ref why)
+        => write!(f, "{}: {}", self.description(), why),
+      TLSError::AlertReceived(ref alert)
+        => write!(f, "{}: {:?}", self.description(), alert),
+      TLSError::WebPKIError(ref err)
+        => write!(f, "{}: {:?}", self.description(), err),
+      TLSError::CorruptMessage
+        | TLSError::NoCertificatesPresented
+        | TLSError::DecryptError
+        => write!(f, "{}", self.description()),
+      _ => write!(f, "{}: {:?}", self.description(), self)
+    }
+  }
+}
+
+impl error::Error for TLSError {
+  fn description(&self) -> &str {
+    match *self {
+      TLSError::InappropriateMessage { .. } => "received unexpected message",
+      TLSError::InappropriateHandshakeMessage { .. } => "received unexpected handshake message",
+      TLSError::CorruptMessage => "received corrupt message",
+      TLSError::CorruptMessagePayload(_) => "received corrupt message",
+      TLSError::NoCertificatesPresented => "peer sent no certificates",
+      TLSError::DecryptError => "cannot decrypt peer's message",
+      TLSError::PeerIncompatibleError(_) => "peer is incompatible",
+      TLSError::PeerMisbehavedError(_) => "peer misbehaved",
+      TLSError::AlertReceived(_) => "received fatal alert",
+      TLSError::WebPKIError(_) => "invalid certificate",
+      TLSError::General(_) => "unexpected error" // (please file a bug)
+    }
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  #[test]
+  fn smoke() {
+    use super::TLSError;
+    use std::error::Error;
+    use msgs::enums::{ContentType, HandshakeType, AlertDescription};
+    extern crate webpki;
+
+    let all = vec![
+      TLSError::InappropriateMessage {
+        expect_types: vec![ ContentType::Alert ],
+        got_type: ContentType::Handshake
+      },
+      TLSError::InappropriateHandshakeMessage {
+        expect_types: vec![ HandshakeType::ClientHello, HandshakeType::Finished ],
+        got_type: HandshakeType::ServerHello
+      },
+      TLSError::CorruptMessage,
+      TLSError::CorruptMessagePayload(ContentType::Alert),
+      TLSError::NoCertificatesPresented,
+      TLSError::DecryptError,
+      TLSError::PeerIncompatibleError("no tls1.2".to_string()),
+      TLSError::PeerMisbehavedError("inconsistent something".to_string()),
+      TLSError::AlertReceived(AlertDescription::ExportRestriction),
+      TLSError::WebPKIError(webpki::Error::ExtensionValueInvalid),
+      TLSError::General("undocumented error".to_string())
+    ];
+
+    for err in all {
+      println!("{:?}:", err);
+      println!("  desc '{}'", err.description());
+      println!("  fmt '{}'", err);
+    }
+  }
 }
