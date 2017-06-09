bc439454d1314d6ec9d534965313643e04dd3d5f
Joseph Birr-Pixton <jpixton@gmail.com>
2017-06-09 19:27:51 +0100

    Move macros to macros.rs, regen enums.rs

--- src/msgs/macros.rs
+++ src/msgs/macros.rs
@@ -3,3 +3,71 @@
 macro_rules! try_ret(
     ($e:expr) => (match $e { Some(e) => e, None => return None })
 );
+
+/// A macro which defines an enum type.
+macro_rules! enum_builder {
+    (@U8
+        EnumName: $enum_name: ident;
+        EnumVal { $( $enum_var: ident => $enum_val: expr ),* }
+    ) => {
+        #[derive(Debug, PartialEq, Eq, Clone, Copy)]
+        pub enum $enum_name {
+            $( $enum_var),*
+            ,Unknown(u8)
+        }
+        impl $enum_name {
+            pub fn get_u8(&self) -> u8 {
+                let x = self.clone();
+                match x {
+                    $( $enum_name::$enum_var => $enum_val),*
+                    ,$enum_name::Unknown(x) => x
+                }
+            }
+        }
+        impl Codec for $enum_name {
+            fn encode(&self, bytes: &mut Vec<u8>) {
+                encode_u8(self.get_u8(), bytes);
+            }
+
+            fn read(r: &mut Reader) -> Option<Self> {
+                Some(match read_u8(r) {
+                    Option::None => return None,
+                    $( Option::Some($enum_val) => $enum_name::$enum_var),*
+                    ,Option::Some(x) => $enum_name::Unknown(x)
+                })
+            }
+        }
+    };
+    (@U16
+        EnumName: $enum_name: ident;
+        EnumVal { $( $enum_var: ident => $enum_val: expr ),* }
+    ) => {
+        #[derive(Debug, PartialEq, Eq, Clone, Copy)]
+        pub enum $enum_name {
+            $( $enum_var),*
+            ,Unknown(u16)
+        }
+        impl $enum_name {
+            pub fn get_u16(&self) -> u16 {
+                let x = self.clone();
+                match x {
+                    $( $enum_name::$enum_var => $enum_val),*
+                    ,$enum_name::Unknown(x) => x
+                }
+            }
+        }
+        impl Codec for $enum_name {
+            fn encode(&self, bytes: &mut Vec<u8>) {
+                encode_u16(self.get_u16(), bytes);
+            }
+
+            fn read(r: &mut Reader) -> Option<Self> {
+                Some(match read_u16(r) {
+                    Option::None => return None,
+                    $( Option::Some($enum_val) => $enum_name::$enum_var),*
+                    ,Option::Some(x) => $enum_name::Unknown(x)
+                })
+            }
+        }
+    };
+}
