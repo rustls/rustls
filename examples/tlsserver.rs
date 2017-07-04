178d1ca129dbc60adb9cb49259a5ee28b46a18cd
Joseph Birr-Pixton <jpixton@gmail.com>
2017-07-05 00:05:33 +0100

    rustfmt ate tlsserver's help message

--- examples/tlsserver.rs
+++ examples/tlsserver.rs
@@ -360,62 +360,46 @@ const USAGE: &'static str =
     "
 Runs a TLS server on :PORT.  The default PORT is 443.
 
-`echo' mode means the server echoes \
-     received data on each connection.
+`echo' mode means the server echoes received data on each connection.
 
-`http' mode means the server blindly sends a HTTP \
-     response on each connection.
+`http' mode means the server blindly sends a HTTP response on each
+connection.
 
-`forward' means the server forwards plaintext to a connection \
-     made to
+`forward' means the server forwards plaintext to a connection made to
 localhost:fport.
 
-`--certs' names the full certificate chain, `--key' provides the \
-     RSA private
-key.
+`--certs' names the full certificate chain, `--key' provides the
+RSA private key.
 
 Usage:
   tlsserver --certs CERTFILE --key KEYFILE [--suite SUITE ...] \
      [--proto PROTO ...] [options] echo
-  tlsserver --certs CERTFILE --key KEYFILE [--suite SUITE \
-     ...] [--proto PROTO ...] [options] http
-  tlsserver --certs CERTFILE --key KEYFILE [--suite \
-     SUITE ...] [--proto PROTO ...] [options] forward <fport>
+  tlsserver --certs CERTFILE --key KEYFILE [--suite SUITE ...] \
+     [--proto PROTO ...] [options] http
+  tlsserver --certs CERTFILE --key KEYFILE [--suite SUITE ...] \
+     [--proto PROTO ...] [options] forward <fport>
   tlsserver (--version | -v)
-  \
-     tlsserver (--help | -h)
+  tlsserver (--help | -h)
 
 Options:
     -p, --port PORT     Listen on PORT [default: 443].
-    \
-     --certs CERTFILE    Read server certificates from CERTFILE.
-                        This \
-     should contain PEM-format certificates
-                        in the right order (the first \
-     certificate should
+    --certs CERTFILE    Read server certificates from CERTFILE.
+                        This should contain PEM-format certificates
+                        in the right order (the first certificate should
                         certify KEYFILE, the last should be a root CA).
-    \
-     --key KEYFILE       Read private key from KEYFILE.  This should be a RSA
-                        \
-     private key or PKCS8-encoded private key, in PEM format.
-    --auth CERTFILE     Enable client authentication, and accept \
-     certificates
+    --key KEYFILE       Read private key from KEYFILE.  This should be a RSA
+                        private key or PKCS8-encoded private key, in PEM format.
+    --auth CERTFILE     Enable client authentication, and accept certificates
                         signed by those roots provided in CERTFILE.
-    \
-     --require-auth      Send a fatal alert if the client does not complete client
-                        \
-     authentication.
+    --require-auth      Send a fatal alert if the client does not complete client
+                        authentication.
     --resumption        Support session resumption.
-    --tickets           \
-     Support tickets.
+    --tickets           Support tickets.
     --suite SUITE       Disable default cipher suite list, and use
-                        \
-     SUITE instead.  May be used multiple times.
-    --proto PROTOCOL    Negotiate PROTOCOL using \
-     ALPN.
+                        SUITE instead.  May be used multiple times.
+    --proto PROTOCOL    Negotiate PROTOCOL using ALPN.
                         May be used multiple times.
-    --verbose           Emit log \
-     output.
+    --verbose           Emit log output.
     --version, -v       Show tool version.
     --help, -h          Show this screen.
 ";
