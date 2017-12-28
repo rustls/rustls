6c8ebc9f1684252b547b02902ec72c98408175f3
Joseph Birr-Pixton <jpixton@gmail.com>
2017-12-28 17:28:48 +0000

    check.py: help clean up DisabledTests

--- bogo/check.py
+++ bogo/check.py
@@ -1,16 +1,19 @@
 import re
 import json
+import fnmatch
 
 config = json.load(open('config.json'))
 test_error_set = set(config['TestErrorMap'].keys())
+obsolete_disabled_tests = set()
 
 all_tests = set()
 failing_tests = set()
 unimpl_tests = set()
+disabled_tests = set()
 passed_tests = set()
 
 for line in open('out'):
-    m = re.match('^(PASSED|UNIMPLEMENTED|FAILED) \((.*)\)$', line.strip())
+    m = re.match('^(PASSED|UNIMPLEMENTED|FAILED|DISABLED) \((.*)\)$', line.strip())
     if m:
         status, name = m.groups()
         if name in test_error_set:
@@ -20,8 +23,18 @@ for line in open('out'):
             failing_tests.add(name)
         elif status == 'UNIMPLEMENTED':
             unimpl_tests.add(name)
+        elif status == 'DISABLED':
+            disabled_tests.add(name)
         elif status == 'PASSED':
             passed_tests.add(name)
+
+if disabled_tests:
+    for disabled_glob in sorted(config['DisabledTests'].keys()):
+        tests_matching_glob = fnmatch.filter(disabled_tests, disabled_glob)
+        if not tests_matching_glob:
+            print 'DisabledTests glob', disabled_glob, 'matches no tests'
+else:
+    print '(DisabledTests unchecked)'
 
 print len(all_tests), 'total tests'
 print len(passed_tests), 'passed'
