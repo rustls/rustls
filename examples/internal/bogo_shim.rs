3912f0b701f2d6d62a33e9e964c4b194ec70e20e
Joseph Birr-Pixton <jpixton@gmail.com>
2018-06-01 23:41:13 +0100

    Enable ECDSA bogo tests

--- examples/internal/bogo_shim.rs
+++ examples/internal/bogo_shim.rs
@@ -124,11 +124,6 @@ fn load_cert(filename: &str) -> Vec<rustls::Certificate> {
 }
 
 fn load_key(filename: &str) -> rustls::PrivateKey {
-    if filename.contains("ecdsa") {
-        println_err!("No ECDSA key support");
-        process::exit(BOGO_NACK);
-    }
-
     let keyfile = fs::File::open(filename).expect("cannot open private key file");
     let mut reader = BufReader::new(keyfile);
     let keys = rustls::internal::pemfile::pkcs8_private_keys(&mut reader).unwrap();
