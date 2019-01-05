b490b5efe558b79c5ba79ef468eb021c95252507
Benjamin Saunders <ben.e.saunders@gmail.com>
2019-01-04 17:14:03 -0800

    Fix malformed resumption handshake under QUIC

--- src/client/hs.rs
+++ src/client/hs.rs
@@ -337,6 +337,8 @@ fn emit_client_hello_for_retry(sess: &mut ClientSessionImpl,
         )));
     }
 
+    // Extra extensions must be placed before the PSK extension
+    exts.extend(handshake.extra_exts.iter().cloned());
 
     let fill_in_binder = if support_tls13 && sess.config.enable_tickets &&
                             resume_version == ProtocolVersion::TLSv1_3 &&
@@ -390,8 +392,6 @@ fn emit_client_hello_for_retry(sess: &mut ClientSessionImpl,
     } else {
         false
     };
-
-    exts.extend(handshake.extra_exts.iter().cloned());
 
     // Note what extensions we sent.
     hello.sent_extensions = exts.iter()
