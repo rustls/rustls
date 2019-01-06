7481542c313cdef222c4d34b43d085726595a113
Benjamin Saunders <ben.e.saunders@gmail.com>
2019-01-06 15:07:50 -0800

    Test QUIC APIs

--- tests/api.rs
+++ tests/api.rs
@@ -21,6 +21,8 @@ use rustls::{Certificate, PrivateKey};
 use rustls::internal::pemfile;
 use rustls::{RootCertStore, NoClientAuth, AllowAnyAuthenticatedClient};
 use rustls::KeyLog;
+#[cfg(feature = "quic")]
+use rustls::quic::{self, QuicExt, ClientQuicExt, ServerQuicExt};
 
 extern crate webpki;
 
@@ -1875,4 +1877,112 @@ fn tls13_stateless_resumption() {
     assert_eq!(storage.puts(), 0);
     assert_eq!(storage.gets(), 0);
     assert_eq!(storage.takes(), 0);
+}
+
+#[test]
+#[cfg(feature = "quic")]
+fn quic_handshake() {
+    // Returns the sender's next secrets to use, or the receiver's error.
+    fn step(send: &mut Session, recv: &mut Session) -> Result<Option<quic::Secrets>, TLSError> {
+        let mut buf = Vec::new();
+        let secrets = loop {
+            let prev = buf.len();
+            if let Some(x) = send.write_hs(&mut buf) {
+                break Some(x);
+            }
+            if prev == buf.len() {
+                break None;
+            }
+        };
+        if let Err(e) = recv.read_hs(&buf) {
+            return Err(e);
+        } else {
+            assert_eq!(recv.get_alert(), None);
+        }
+        Ok(secrets)
+    }
+
+    let kt = KeyType::RSA;
+    let mut client_config = make_client_config(kt);
+    client_config.versions = vec![ProtocolVersion::TLSv1_3];
+    client_config.enable_early_data = true;
+    let client_config = Arc::new(client_config);
+    let mut server_config = make_server_config(kt);
+    server_config.versions = vec![ProtocolVersion::TLSv1_3];
+    let server_config = Arc::new(server_config);
+    let client_params = &b"client params"[..];
+    let server_params = &b"server params"[..];
+
+    // full handshake
+    let mut client =
+        ClientSession::new_quic(&client_config, dns_name("localhost"), client_params.into());
+    let mut server = ServerSession::new_quic(&server_config, server_params.into());
+    let client_initial = step(&mut client, &mut server).unwrap();
+    assert!(client_initial.is_none());
+    assert!(client.get_early_secret().is_none());
+    assert_eq!(server.get_quic_transport_parameters(), Some(client_params));
+    let server_hs = step(&mut server, &mut client).unwrap().unwrap();
+    assert!(server.get_early_secret().is_none());
+    let client_hs = step(&mut client, &mut server).unwrap().unwrap();
+    assert_eq!(server_hs, client_hs);
+    assert!(client.is_handshaking());
+    let server_1rtt = step(&mut server, &mut client).unwrap().unwrap();
+    assert!(!client.is_handshaking());
+    assert_eq!(client.get_quic_transport_parameters(), Some(server_params));
+    assert!(server.is_handshaking());
+    let client_1rtt = step(&mut client, &mut server).unwrap().unwrap();
+    assert!(!server.is_handshaking());
+    assert_eq!(server_1rtt, client_1rtt);
+    assert_ne!(server_hs, server_1rtt);
+    assert!(step(&mut client, &mut server).unwrap().is_none());
+    assert!(step(&mut server, &mut client).unwrap().is_none());
+
+    // key update
+    let initial = quic::Secrets {
+        // Constant dummy values for reproducibility
+        client: vec![
+            0xb8, 0x76, 0x77, 0x08, 0xf8, 0x77, 0x23, 0x58, 0xa6, 0xea, 0x9f, 0xc4, 0x3e, 0x4a,
+            0xdd, 0x2c, 0x96, 0x1b, 0x3f, 0x52, 0x87, 0xa6, 0xd1, 0x46, 0x7e, 0xe0, 0xae, 0xab,
+            0x33, 0x72, 0x4d, 0xbf,
+        ],
+        server: vec![
+            0x42, 0xdc, 0x97, 0x21, 0x40, 0xe0, 0xf2, 0xe3, 0x98, 0x45, 0xb7, 0x67, 0x61, 0x34,
+            0x39, 0xdc, 0x67, 0x58, 0xca, 0x43, 0x25, 0x9b, 0x87, 0x85, 0x06, 0x82, 0x4e, 0xb1,
+            0xe4, 0x38, 0xd8, 0x55,
+        ],
+    };
+    let updated = client.update_secrets(&initial.client, &initial.server);
+    // The expected values will need to be updated if the negotiated hash function changes.
+    assert_eq!(
+        updated.client,
+        &[
+            101, 250, 130, 179, 97, 208, 160, 166, 213, 90, 196, 212, 96, 49, 190, 24, 237, 225,
+            68, 97, 141, 123, 162, 108, 231, 21, 255, 184, 49, 245, 178, 148
+        ]
+    );
+    assert_eq!(
+        updated.server,
+        &[
+            171, 127, 244, 22, 119, 205, 252, 100, 179, 94, 91, 45, 99, 82, 236, 124, 44, 251, 63,
+            57, 94, 215, 175, 138, 178, 161, 97, 80, 51, 250, 107, 85
+        ]
+    );
+
+    // failed handshake
+    let mut client = ClientSession::new_quic(
+        &client_config,
+        dns_name("example.com"),
+        client_params.into(),
+    );
+    let mut server = ServerSession::new_quic(&server_config, server_params.into());
+    step(&mut client, &mut server).unwrap();
+    step(&mut server, &mut client).unwrap().unwrap();
+    step(&mut server, &mut client).unwrap_err();
+    assert_eq!(
+        client.get_alert(),
+        Some(rustls::internal::msgs::enums::AlertDescription::BadCertificate)
+    );
+
+    // TODO: We can't easily test 0-RTT secret availability because we don't yet support 0-RTT on
+    // the server.
 }
