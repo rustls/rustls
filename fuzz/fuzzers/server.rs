d6176ed6ac63c402fadac149141870c257825251
Joseph Birr-Pixton <jpixton@gmail.com>
2020-06-14 10:57:56 +0100

    Update fuzz targets
    Prepare to run them in CI

--- fuzz/fuzzers/server.rs
+++ fuzz/fuzzers/server.rs
@@ -2,12 +2,12 @@
 #[macro_use] extern crate libfuzzer_sys;
 extern crate rustls;
 
-use rustls::{ServerConfig, ServerSession, Session};
+use rustls::{ServerConfig, ServerSession, Session, NoClientAuth};
 use std::io;
 use std::sync::Arc;
 
 fuzz_target!(|data: &[u8]| {
-    let config = Arc::new(ServerConfig::new());
+    let config = Arc::new(ServerConfig::new(NoClientAuth::new()));
     let mut server= ServerSession::new(&config);
     let _ = server.read_tls(&mut io::Cursor::new(data));
 });
