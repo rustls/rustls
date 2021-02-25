4139a092463b8f210fbbf0abffe4106081f9302c
Robert Sayre <sayrer@gmail.com>
2021-02-24 16:21:36 -0800

    Fix names and comments in ECH test code.

--- rustls/src/msgs/ech_test.rs
+++ rustls/src/msgs/ech_test.rs
@@ -4,13 +4,13 @@ use crate::msgs::handshake::ECHConfigs;
 
 #[test]
 fn test_echconfig_serialization() {
-    // An ECHConfig record from Cloudflare for
-    let base64_esni = "AEf+CQBDABNjbG91ZGZsYXJlLWVzbmkuY29tACCD91Ovu3frIsjhFKo0I1fPd/a09nzKMrjC9GZV3NvrfQAgAAQAAQABAAAAAA==";
-    let bytes = base64::decode(&base64_esni).unwrap();
+    // An ECHConfig record from Cloudflare for "crypto.cloudflare.com"
+    let base64_echconfig = "AEf+CQBDABNjbG91ZGZsYXJlLWVzbmkuY29tACCD91Ovu3frIsjhFKo0I1fPd/a09nzKMrjC9GZV3NvrfQAgAAQAAQABAAAAAA==";
+    let bytes = base64::decode(&base64_echconfig).unwrap();
     let records = ECHConfigs::read(&mut Reader::init(&bytes)).unwrap();
     let name = String::from_utf8(records[0].contents.public_name.clone().into_inner()).unwrap();
     assert_eq!("cloudflare-esni.com", name.as_str());
     let mut output = Vec::new();
     records.encode(&mut output);
-    assert_eq!(base64_esni, base64::encode(&output));
+    assert_eq!(base64_echconfig, base64::encode(&output));
 }
