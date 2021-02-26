26e86ad3b629548af0d675ada5cd0827f8d5bb95
Robert Sayre <sayrer@gmail.com>
2021-02-26 12:20:02 -0800

    Add more ECH tests.

--- rustls/src/msgs/ech.rs
+++ rustls/src/msgs/ech.rs
@@ -3,36 +3,42 @@ use crate::msgs::handshake::{ECHConfigContents, ECHConfig};
 use hpke::{kem, Kem};
 use hpke::kex::Serializable;
 
+// TODO: delegate to ring?
+use rand::{rngs::StdRng, SeedableRng};
+
 pub type HPKEPrivateKey = Vec<u8>;
 pub type HPKEPublicKey = Vec<u8>;
+
+#[derive(Clone, Debug)]
 pub struct HPKEKeyPair {
     pub private_key: HPKEPrivateKey,
     pub public_key: HPKEPublicKey,
 }
 
-// TODO: delegate to ring?
-use rand::{rngs::StdRng, SeedableRng};
-pub fn gen_hpke_keypair(kem: KEM) -> HPKEKeyPair {
-    let mut csprng = StdRng::from_entropy();
-    let (private_key, public_key) = match kem {
-        KEM::DHKEM_P256_HKDF_SHA256 => {
-            let (private, public) = kem::DhP256HkdfSha256::gen_keypair(&mut csprng);
-            (private.to_bytes().as_slice().to_vec(), public.to_bytes().as_slice().to_vec())
-        },
-        KEM::DHKEM_P384_HKDF_SHA384 => unimplemented!(),
-        KEM::DHKEM_P521_HKDF_SHA512 => unimplemented!(),
-        KEM::DHKEM_X25519_HKDF_SHA256 => {
-            let (private, public) = kem::X25519HkdfSha256::gen_keypair(&mut csprng);
-            (private.to_bytes().as_slice().to_vec(), public.to_bytes().as_slice().to_vec())
+impl HPKEKeyPair {
+    pub fn new(kem: KEM) -> HPKEKeyPair {
+        let mut csprng = StdRng::from_entropy();
+        let (private_key, public_key) = match kem {
+            KEM::DHKEM_P256_HKDF_SHA256 => {
+                let (private, public) = kem::DhP256HkdfSha256::gen_keypair(&mut csprng);
+                (private.to_bytes().as_slice().to_vec(), public.to_bytes().as_slice().to_vec())
+            },
+            KEM::DHKEM_P384_HKDF_SHA384 => unimplemented!(),
+            KEM::DHKEM_P521_HKDF_SHA512 => unimplemented!(),
+            KEM::DHKEM_X25519_HKDF_SHA256 => {
+                let (private, public) = kem::X25519HkdfSha256::gen_keypair(&mut csprng);
+                (private.to_bytes().as_slice().to_vec(), public.to_bytes().as_slice().to_vec())
+            }
+            KEM::DHKEM_X448_HKDF_SHA512 => unimplemented!(),
+            _ => unreachable!(),
+        };
+        HPKEKeyPair {
+            private_key,
+            public_key,
         }
-        KEM::DHKEM_X448_HKDF_SHA512 => unimplemented!(),
-        _ => unreachable!(),
-    };
-    HPKEKeyPair {
-        private_key,
-        public_key,
     }
 }
+
 
 /// A private key paired with an ECHConfig, which contains the corresponding public key.
 pub struct ECHKey {
--- rustls/src/msgs/ech_test.rs
+++ rustls/src/msgs/ech_test.rs
@@ -2,6 +2,8 @@ use base64;
 use crate::msgs::codec::{Codec, Reader};
 use crate::msgs::handshake::{ECHConfigs, ECHConfig};
 use crate::msgs::enums::{ECHVersion, KEM, KDF, AEAD};
+use crate::msgs::ech::{HPKEKeyPair, ECHKey};
+use std::convert::TryFrom;
 
 #[test]
 fn test_echconfig_serialization() {
@@ -21,4 +23,48 @@ fn test_echconfig_serialization() {
     let mut output = Vec::new();
     configs.encode(&mut output);
     assert_eq!(base64_echconfigs, base64::encode(&output));
+}
+
+#[test]
+fn test_gen_p256() {
+    let p256 = HPKEKeyPair::new(KEM::DHKEM_P256_HKDF_SHA256);
+    assert_eq!(p256.private_key.len(), 32);
+    assert_eq!(p256.public_key.len(), 65);
+}
+
+#[test]
+#[should_panic]
+fn test_gen_p384() {
+    let _p384 = HPKEKeyPair::new(KEM::DHKEM_P384_HKDF_SHA384);
+}
+
+#[test]
+#[should_panic]
+fn test_gen_p521() {
+    let _p521 = HPKEKeyPair::new(KEM::DHKEM_P521_HKDF_SHA512);
+}
+
+#[test]
+fn test_gen_x25519() {
+    let x25519 = HPKEKeyPair::new(KEM::DHKEM_X25519_HKDF_SHA256);
+    assert_eq!(x25519.private_key.len(), 32);
+    assert_eq!(x25519.public_key.len(), 32);
+}
+
+#[test]
+#[should_panic]
+fn test_gen_x448() {
+    let _x448 = HPKEKeyPair::new(KEM::DHKEM_X448_HKDF_SHA512);
+}
+
+#[test]
+fn test_create_default_ech_config() {
+    let x25519 = HPKEKeyPair::new(KEM::DHKEM_X25519_HKDF_SHA256);
+    let domain = webpki::DNSNameRef::try_from_ascii_str("example.com").unwrap();
+    let key = ECHKey::new(x25519.clone(), domain);
+    assert_eq!(key.private_key, x25519.private_key);
+    assert_eq!(key.config.version, ECHVersion::V9);
+    assert_eq!(key.config.contents.hpke_kem_id, KEM::DHKEM_X25519_HKDF_SHA256);
+    assert_eq!(key.config.contents.ech_cipher_suites[0].hpke_aead_id, AEAD::AES_128_GCM);
+    assert_eq!(key.config.contents.ech_cipher_suites[0].hpke_kdf_id, KDF::HKDF_SHA256);
 }
