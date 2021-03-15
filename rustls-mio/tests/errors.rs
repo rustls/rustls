fd6d63d032552564dadbfbe409fb37105455b577
ctz <jpixton@gmail.com>
2021-03-15 20:00:53 +0000

    Delete no_ecdhe test
    This doesn't work, and is really a test of whether `openssl s_server -no_ecdhe` works, which is not very interesting to us on balance.
    
    Bogo provides am error handling tests for this case.

--- rustls-mio/tests/errors.rs
+++ rustls-mio/tests/errors.rs
@@ -5,28 +5,6 @@ mod common;
 use crate::common::OpenSSLServer;
 
 #[test]
-fn no_ecdhe() {
-    let test_ca = common::new_test_ca();
-
-    let mut server = OpenSSLServer::new_rsa(test_ca.path(), 8010);
-    if common::openssl_server_supports_no_echde() {
-        server.arg("-no_ecdhe");
-    } else {
-        common::skipped("openssl s_server -no_ecdhe");
-        return;
-    }
-    server.run();
-
-    server
-        .client()
-        .verbose()
-        .fails()
-        .expect_log("TLS alert received:")
-        .expect(r"TLS error: AlertReceived\(HandshakeFailure\)")
-        .go();
-}
-
-#[test]
 fn tls11_only() {
     let test_ca = common::new_test_ca();
 
