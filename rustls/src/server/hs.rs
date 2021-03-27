ccf6db72b803ee0f18aa4bc810a33b9f0e4623f5
Dirkjan Ochtman <dirkjan@ochtman.nl>
2021-03-27 20:21:57 +0100

    server: simplify how SigningKey arguments are passed

--- rustls/src/server/hs.rs
+++ rustls/src/server/hs.rs
@@ -31,8 +31,6 @@ use webpki;
 use crate::server::common::{HandshakeDetails, ServerKXDetails};
 use crate::server::{tls12, tls13};
 
-use std::sync::Arc;
-
 pub type NextState = Box<dyn State + Send + Sync>;
 pub type NextStateOrError = Result<NextState, Error>;
 
@@ -419,7 +417,7 @@ impl ExpectClientHello {
         sess: &mut ServerSession,
         sigschemes: Vec<SignatureScheme>,
         skxg: &'static kx::SupportedKxGroup,
-        signing_key: &Arc<dyn sign::SigningKey>,
+        signing_key: &dyn sign::SigningKey,
         randoms: &SessionRandoms,
     ) -> Result<kx::KeyExchange, Error> {
         let kx = kx::KeyExchange::start(skxg)
@@ -906,7 +904,7 @@ impl State for ExpectClientHello {
         if let Some(ocsp_response) = ocsp_response {
             self.emit_cert_status(sess, ocsp_response);
         }
-        let kx = self.emit_server_kx(sess, sigschemes, group, &certkey.key, &randoms)?;
+        let kx = self.emit_server_kx(sess, sigschemes, group, &*certkey.key, &randoms)?;
         let doing_client_auth = self.emit_certificate_req(sess)?;
         self.emit_server_hello_done(sess);
 
