0f4d94a22d20665ee5f3f9b552b5d26443e13f18
Dirkjan Ochtman <dirkjan@ochtman.nl>
2021-03-29 15:12:22 +0200

    server: thread hash_at_server_fin through states

--- rustls/src/server/common.rs
+++ rustls/src/server/common.rs
@@ -3,12 +3,10 @@ use crate::key;
 use crate::kx;
 use crate::msgs::handshake::SessionID;
 
-use ring::digest;
 use std::mem;
 
 pub struct HandshakeDetails {
     pub transcript: hash_hs::HandshakeHash,
-    pub hash_at_server_fin: Option<digest::Digest>,
     pub session_id: SessionID,
 }
 
@@ -16,7 +14,6 @@ impl HandshakeDetails {
     pub fn new() -> HandshakeDetails {
         HandshakeDetails {
             transcript: hash_hs::HandshakeHash::new(),
-            hash_at_server_fin: None,
             session_id: SessionID::empty(),
         }
     }
