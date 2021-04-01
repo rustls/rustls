87caac07d4603b3eddce752381a23d38b42dfcc0
Joseph Birr-Pixton <jpixton@gmail.com>
2021-04-01 11:58:33 +0100

    Use cargo-fuzz to find un-roundtripable messages

--- fuzz/fuzzers/message.rs
+++ fuzz/fuzzers/message.rs
@@ -7,6 +7,11 @@ use rustls::internal::msgs::codec::{Reader, Codec};
 
 fuzz_target!(|data: &[u8]| {
     let mut rdr = Reader::init(data);
-    Message::read(&mut rdr)
-        .map(|mut msg| msg.decode_payload());
+    if let Some(mut m) = Message::read(&mut rdr) {
+        m.decode_payload();
+        //println!("msg = {:#?}", m);
+        let enc = m.get_encoding();
+        //println!("data = {:?}", &data[..rdr.used()]);
+        assert_eq!(enc, data[..rdr.used()]);
+    }
 });
