a372b6ade3b4a3d8eac233c675cabddf07890257
Dirkjan Ochtman <dirkjan@ochtman.nl>
2021-04-19 13:56:23 +0200

    Apply clippy suggestion

--- rustls/src/server/tls12.rs
+++ rustls/src/server/tls12.rs
@@ -535,9 +535,9 @@ impl hs::State for ExpectCertificate {
                 cx.config
                     .verifier
                     .verify_client_cert(end_entity, intermediates, cx.data.get_sni(), now)
-                    .or_else(|err| {
+                    .map_err(|err| {
                         hs::incompatible(&mut cx.common, "certificate invalid");
-                        Err(err)
+                        err
                     })?;
 
                 Some(cert_chain)
