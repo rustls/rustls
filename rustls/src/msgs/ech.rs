3cc5f85c3aafc22bda4560b17345f2d0a3407c18
RS <sayrer@gmail.com>
2021-05-12 13:23:15 -0700

    fmt

--- rustls/src/msgs/ech.rs
+++ rustls/src/msgs/ech.rs
@@ -1,13 +1,13 @@
-use crate::msgs::enums::ExtensionType;
-use crate::msgs::handshake::{HpkeKeyConfig, ClientHelloPayload, ClientExtension, SessionID};
 use crate::msgs::codec::*;
+use crate::msgs::enums::ExtensionType;
+use crate::msgs::handshake::ClientExtension::EchOuterExtensions;
+use crate::msgs::handshake::{ClientExtension, ClientHelloPayload, HpkeKeyConfig, SessionID};
 use crate::rand;
 use crate::Error;
 use hpke_rs::prelude::*;
 use hpke_rs::{Hpke, Mode};
-use webpki::DnsName;
 use std::cmp::Ordering;
-use crate::msgs::handshake::ClientExtension::EchOuterExtensions;
+use webpki::DnsName;
 
 #[allow(dead_code)]
 /// ECH data that's reused across HRR.
@@ -50,23 +50,37 @@ impl EchHrrContext {
 }
 
 #[allow(dead_code)]
-fn encode_inner_hello(mut hello: ClientHelloPayload, outer_exts: &Vec<ExtensionType>) -> (ClientHelloPayload, Vec<u8>) {
-
+fn encode_inner_hello(
+    mut hello: ClientHelloPayload,
+    outer_exts: &Vec<ExtensionType>,
+) -> (ClientHelloPayload, Vec<u8>) {
     hello.extensions.sort_by(|a, b| {
         if outer_exts.contains(&a.get_type()) {
             Ordering::Greater
-        } else if outer_exts.contains(&b.get_type())  {
+        } else if outer_exts.contains(&b.get_type()) {
             Ordering::Less
         } else {
             Ordering::Equal
         }
     });
 
-    let range = hello.extensions.iter().rev().take_while(|el| outer_exts.contains(&el.get_type())).collect::<Vec<&ClientExtension>>().len();
-    let outer: Vec<ClientExtension> = hello.extensions.drain(hello.extensions.len() - range..).collect();
-    let outer_extensions = EchOuterExtensions(outer.iter().map(|ext| {
-        ext.get_type()
-    }).collect());
+    let range = hello
+        .extensions
+        .iter()
+        .rev()
+        .take_while(|el| outer_exts.contains(&el.get_type()))
+        .collect::<Vec<&ClientExtension>>()
+        .len();
+    let outer: Vec<ClientExtension> = hello
+        .extensions
+        .drain(hello.extensions.len() - range..)
+        .collect();
+    let outer_extensions = EchOuterExtensions(
+        outer
+            .iter()
+            .map(|ext| ext.get_type())
+            .collect(),
+    );
 
     let legacy_session_id = hello.session_id;
     hello.session_id = SessionID::empty();
@@ -84,14 +98,14 @@ fn encode_inner_hello(mut hello: ClientHelloPayload, outer_exts: &Vec<ExtensionT
 #[cfg(test)]
 mod test {
     use super::*;
-    use crate::msgs::base::{PayloadU16, PayloadU8, Payload};
+    use crate::internal::msgs::enums::ExtensionType::ECPointFormats;
+    use crate::msgs::base::{Payload, PayloadU8, PayloadU16};
     use crate::msgs::codec::{Codec, Reader};
+    use crate::msgs::enums::ExtensionType::{EllipticCurves, KeyShare};
     use crate::msgs::enums::*;
     use crate::msgs::handshake::*;
-    use base64;
     use crate::ProtocolVersion;
-    use crate::msgs::enums::ExtensionType::{KeyShare, EllipticCurves};
-    use crate::internal::msgs::enums::ExtensionType::ECPointFormats;
+    use base64;
     use webpki::DnsNameRef;
 
     #[test]
@@ -110,7 +124,7 @@ mod test {
                 .clone()
                 .into_inner(),
         )
-            .unwrap();
+        .unwrap();
         assert_eq!("cloudflare-esni.com", name.as_str());
         assert_eq!(
             config
@@ -161,7 +175,9 @@ mod test {
                 ClientExtension::ECPointFormats(ECPointFormatList::supported()),
                 ClientExtension::NamedGroups(vec![NamedGroup::X25519]),
                 ClientExtension::SignatureAlgorithms(vec![SignatureScheme::ECDSA_NISTP256_SHA256]),
-                ClientExtension::make_sni(DnsNameRef::try_from_ascii_str("inner-sni.example.com").unwrap()),
+                ClientExtension::make_sni(
+                    DnsNameRef::try_from_ascii_str("inner-sni.example.com").unwrap(),
+                ),
                 ClientExtension::SessionTicketRequest,
                 ClientExtension::SessionTicketOffer(Payload(vec![])),
                 ClientExtension::Protocols(vec![PayloadU8(vec![0])]),
@@ -208,11 +224,14 @@ mod test {
         // The compressed extensions, plus one for the outer_extensions field.
         let expected_length = original_ext_length - outer_exts.len() + 1;
         assert_eq!(decoded.extensions.len(), expected_length);
-        let decoded_outer = decoded.find_extension(ExtensionType::EchOuterExtensions).unwrap();
+        let decoded_outer = decoded
+            .find_extension(ExtensionType::EchOuterExtensions)
+            .unwrap();
         let outers = match decoded_outer {
             EchOuterExtensions(outer_exts) => Some(outer_exts),
             _ => None,
-        }.unwrap();
+        }
+        .unwrap();
         assert_eq!(outers.len(), 3);
         for ext_type in outers.iter() {
             assert!(outer_exts.contains(ext_type));
