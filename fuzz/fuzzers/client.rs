7951aca9f2e1a2d19718117eda81fb9c4fe3f050
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2021-06-26 02:19:37 -0700

    Fix fuzzers.

--- fuzz/fuzzers/client.rs
+++ fuzz/fuzzers/client.rs
@@ -1,20 +1,23 @@
 #![no_main]
-#[macro_use] extern crate libfuzzer_sys;
+#[macro_use]
+extern crate libfuzzer_sys;
 extern crate rustls;
 extern crate webpki;
 
-use rustls::{ConfigBuilder, ClientConnection, Connection, RootCertStore};
+use rustls::{config_builder_with_safe_defaults, ClientConnection, Connection, RootCertStore};
+use std::convert::TryInto;
 use std::io;
 use std::sync::Arc;
-use std::convert::TryInto;
 
 fuzz_target!(|data: &[u8]| {
     let root_store = RootCertStore::empty();
-    let config = Arc::new(ConfigBuilder::with_safe_defaults()
-        .for_client()
-        .unwrap()
-        .with_root_certificates(root_store, &[])
-        .with_no_client_auth());
+    let config = Arc::new(
+        config_builder_with_safe_defaults()
+            .for_client()
+            .unwrap()
+            .with_root_certificates(root_store, &[])
+            .with_no_client_auth(),
+    );
     let example_com = "example.com".try_into().unwrap();
     let mut client = ClientConnection::new(config, example_com).unwrap();
     let _ = client.read_tls(&mut io::Cursor::new(data));
