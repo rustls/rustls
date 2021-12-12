b12d27e69ab3c387e7404c61788e05259f14647f
David Cook <divergentdave@gmail.com>
2021-12-12 13:46:27 -0600

    Add read_buf to Stream and StreamOwned impls

--- rustls/src/stream.rs
+++ rustls/src/stream.rs
@@ -72,6 +72,31 @@ where
 
         self.conn.reader().read(buf)
     }
+
+    #[cfg(feature = "read_buf")]
+    fn read_buf(&mut self, buf: &mut std::io::ReadBuf<'_>) -> Result<()> {
+        self.complete_prior_io()?;
+
+        // We call complete_io() in a loop since a single call may read only
+        // a partial packet from the underlying transport. A full packet is
+        // needed to get more plaintext, which we must do if EOF has not been
+        // hit. Otherwise, we will prematurely signal EOF by returning without
+        // writing anything. We determine if EOF has actually been hit by
+        // checking if 0 bytes were read from the underlying transport.
+        while self.conn.wants_read() {
+            let at_eof = self.conn.complete_io(self.sock)?.0 == 0;
+            if at_eof {
+                if let Ok(io_state) = self.conn.process_new_packets() {
+                    if at_eof && io_state.plaintext_bytes_to_read() == 0 {
+                        return Ok(());
+                    }
+                }
+                break;
+            }
+        }
+
+        self.conn.reader().read_buf(buf)
+    }
 }
 
 impl<'a, C, T, S> Write for Stream<'a, C, T>
@@ -182,6 +207,11 @@ where
 {
     fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
         self.as_stream().read(buf)
+    }
+
+    #[cfg(feature = "read_buf")]
+    fn read_buf(&mut self, buf: &mut std::io::ReadBuf<'_>) -> Result<()> {
+        self.as_stream().read_buf(buf)
     }
 }
 
