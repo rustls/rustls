8ed09b6b3c6b3e5b5108a285f8bb60d152ff5e69
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2022-05-29 12:23:27 -0700

    Convert fuzzers/fragment.rs to modern import style

--- fuzz/fuzzers/fragment.rs
+++ fuzz/fuzzers/fragment.rs
@@ -5,27 +5,27 @@ extern crate rustls;
 
 use rustls::internal::msgs::base::Payload;
 use rustls::internal::msgs::codec::Reader;
-use rustls::internal::msgs::fragmenter;
-use rustls::internal::msgs::message;
+use rustls::internal::msgs::fragmenter::MessageFragmenter;
+use rustls::internal::msgs::message::{Message, OpaqueMessage, PlainMessage};
 use std::convert::TryFrom;
 
 fuzz_target!(|data: &[u8]| {
     let mut rdr = Reader::init(data);
-    let msg = match message::OpaqueMessage::read(&mut rdr) {
+    let msg = match OpaqueMessage::read(&mut rdr) {
         Ok(msg) => msg,
         Err(_) => return,
     };
 
-    let msg = match message::Message::try_from(msg.into_plain_message()) {
+    let msg = match Message::try_from(msg.into_plain_message()) {
         Ok(msg) => msg,
         Err(_) => return,
     };
 
-    let mut frg = fragmenter::MessageFragmenter::default();
+    let mut frg = MessageFragmenter::default();
     frg.set_max_fragment_size(Some(32))
         .unwrap();
-    for msg in frg.fragment_message(&message::PlainMessage::from(msg)) {
-        message::Message::try_from(message::PlainMessage {
+    for msg in frg.fragment_message(&PlainMessage::from(msg)) {
+        Message::try_from(PlainMessage {
             typ: msg.typ,
             version: msg.version,
             payload: Payload(msg.payload.to_vec()),
