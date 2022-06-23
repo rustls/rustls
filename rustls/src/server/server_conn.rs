88a2da6d444fe8c8992b885c45ccf15a40e37774
glendc <glen.decauwsemaecker@otainsight.com>
2022-06-23 23:16:49 +0200

    further improve the 'alpn' method docs
    applying the review comments by @djc

--- rustls/src/server/server_conn.rs
+++ rustls/src/server/server_conn.rs
@@ -155,19 +155,23 @@ impl<'a> ClientHello<'a> {
     ///
     /// Returns `None` if the client did not include an ALPN extension
     ///
-    /// ALPN (Application Layer Protocol Negotiation) contains a set of identifiers
-    /// which are encoded as a set of octet values, which could be UTF-8 encoded.
-    /// It's a form of TLS extension allowing the client to provide an externally visible
-    /// marker for the application-layer protocol associated with the TLS connection.
+    /// Application Layer Protocol Negotiation (ALPN) is a TLS extension that lets a client
+    /// submit a set of identifiers that each a represent an application-layer protocol.
+    /// The server will then pick its preferred protocol from the set submitted by the client.
+    /// Each identifier is represented as a byte array, although common values are often ASCII-encoded.
+    /// See the official specifications at <https://datatracker.ietf.org/doc/html/rfc7301> for more information on ALPN.
     ///
-    /// Common examples of ALPN identifiers given by web clients are "http/1.1", "h2" and "h3",
-    /// and are used to hint to the web server that these HTTP protocols are supported by the
-    /// web client can be established if desired.
+    /// Common examples of ALPN identifiers given by web clients are "http/1.1" and "h2".
+    /// These indicate that the web client supports each of these HTTP protocols,
+    /// and it is the server that will respond with one of these two as the chosen HTTP protocol.
+    ///
+    /// On the server side you can configure the ALPN protocols supported by your server using `ServerConfig::alpn_protocols`,
+    /// it are the protocols that it has in common with the ones defined in the `ClientHello` message which will be sent
+    /// back to the client as part of the handshake phase.
     ///
     /// You can find a list of many other possible identifier values at
-    /// <https://www.iana.org/assignments/tls-extensiontype-values/tls-extensiontype-values.xhtml#alpn-protocol-ids>,
-    /// note however that this is not an exhaustive list, so please keep in mind
-    /// that other identifiers are possible as well.
+    /// <https://www.iana.org/assignments/tls-extensiontype-values/tls-extensiontype-values.xhtml#alpn-protocol-ids>.
+    /// Please keep in mind that this is not an exhaustive list.
     pub fn alpn(&self) -> Option<impl Iterator<Item = &'a [u8]>> {
         self.alpn.map(|protocols| {
             protocols
