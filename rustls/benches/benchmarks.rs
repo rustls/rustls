bdd1392a50625e6338b5a8a8f4c0ba33db9408bf
Dirkjan Ochtman <dirkjan@ochtman.nl>
2022-12-12 10:39:35 +0100

    Switch from criterion to bencher
    This has less rich output but doesn't have any dependencies, very
    much unlike criterion (which was breaking our minimal versions check).

--- rustls/benches/benchmarks.rs
+++ rustls/benches/benchmarks.rs
@@ -1,8 +1,4 @@
-use criterion::criterion_group;
-use criterion::criterion_main;
-/// Microbenchmarks go here.  Larger benchmarks of (e.g..) protocol
-/// performance go in examples/internal/bench.rs.
-use criterion::Criterion;
+use bencher::{benchmark_group, benchmark_main, Bencher};
 
 #[path = "../tests/common/mod.rs"]
 mod test_utils;
@@ -13,14 +9,12 @@ use rustls::ServerConnection;
 use std::io;
 use std::sync::Arc;
 
-fn bench_ewouldblock(c: &mut Criterion) {
+fn bench_ewouldblock(c: &mut Bencher) {
     let server_config = make_server_config(KeyType::Rsa);
     let mut server = ServerConnection::new(Arc::new(server_config)).unwrap();
     let mut read_ewouldblock = FailsReads::new(io::ErrorKind::WouldBlock);
-    c.bench_function("read_tls with EWOULDBLOCK", move |b| {
-        b.iter(|| server.read_tls(&mut read_ewouldblock))
-    });
+    c.iter(|| server.read_tls(&mut read_ewouldblock));
 }
 
-criterion_group!(benches, bench_ewouldblock);
-criterion_main!(benches);
+benchmark_group!(benches, bench_ewouldblock);
+benchmark_main!(benches);
