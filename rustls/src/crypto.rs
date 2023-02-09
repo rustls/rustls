9525a9f91aae7fee70161b23729e1df6a4e882ae
Dirkjan Ochtman <dirkjan@ochtman.nl>
2023-02-09 14:34:03 +0100

    crypto: use crypto provider for random bytes

--- rustls/src/crypto.rs
+++ rustls/src/crypto.rs
@@ -1,7 +1,20 @@
+use crate::rand::GetRandomFailed;
+
+use ring::rand::{SecureRandom, SystemRandom};
+
 /// Pluggable crypto galore.
-pub trait CryptoProvider: Send + Sync + 'static {}
+pub trait CryptoProvider: Send + Sync + 'static {
+    /// Fill the given buffer with random bytes.
+    fn fill_random(buf: &mut [u8]) -> Result<(), GetRandomFailed>;
+}
 
 /// Default crypto provider.
 pub struct Ring;
 
-impl CryptoProvider for Ring {}
+impl CryptoProvider for Ring {
+    fn fill_random(buf: &mut [u8]) -> Result<(), GetRandomFailed> {
+        SystemRandom::new()
+            .fill(buf)
+            .map_err(|_| GetRandomFailed)
+    }
+}
