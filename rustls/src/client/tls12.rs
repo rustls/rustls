rustls/rustls

commit 0a0bc5fa64fafcba09a67d76af956bb744ea0c32

Clarify Message reading ordering
Co-authored-by: Dirkjan Ochtman <dirkjan@ochtman.nl>

rustls/src/msgs/message.rs
@@ -85,7 +85,20 @@ impl OpaqueMessage {
     /// become valid if we read more data) and invalid data.
     pub fn read(r: &mut Reader) -> Result<Self, MessageError> {
         let typ = ContentType::read(r).ok_or(MessageError::TooShortForHeader)?;
+        // Don't accept any new content-types.
+        if let ContentType::Unknown(_) = typ {
+            return Err(MessageError::InvalidContentType);
+        }
+
         let version = ProtocolVersion::read(r).ok_or(MessageError::TooShortForHeader)?;
+        // Accept only versions 0x03XX for any XX.
+        match version {
+            ProtocolVersion::Unknown(ref v) if (v & 0xff00) != 0x0300 => {
+                return Err(MessageError::UnknownProtocolVersion);
+            }
+            _ => {}
+        };
+
         let len = u16::read(r).ok_or(MessageError::TooShortForHeader)?;
 
         // Reject undersize messages
@@ -99,19 +112,6 @@ impl OpaqueMessage {
         if len >= Self::MAX_PAYLOAD {
             return Err(MessageError::MessageTooLarge);
         }
-
-        // Don't accept any new content-types.
-        if let ContentType::Unknown(_) = typ {
-            return Err(MessageError::InvalidContentType);
-        }
-
-        // Accept only versions 0x03XX for any XX.
-        match version {
-            ProtocolVersion::Unknown(ref v) if (v & 0xff00) != 0x0300 => {
-                return Err(MessageError::UnknownProtocolVersion);
-            }
-            _ => {}
-        };
 
         let mut sub = r
             .sub(len as usize)

commit 90878d72d6d2f266c0df2035aac7eaee2633a658

Clean up import style regressions from #1172

rustls/src/client/tls12.rs
@@ -3,7 +3,6 @@ use crate::conn::{CommonState, ConnectionRandoms, Side, State};
 use crate::enums::ProtocolVersion;
 use crate::error::{Error, InvalidMessage, PeerMisbehaved};
 use crate::hash_hs::HandshakeHash;
-use crate::kx;
 #[cfg(feature = "logging")]
 use crate::log::{debug, trace};
 use crate::msgs::base::{Payload, PayloadU8};
@@ -23,7 +22,7 @@ use crate::suites::PartiallyExtractedSecrets;
 use crate::suites::SupportedCipherSuite;
 use crate::ticketer::TimeBase;
 use crate::tls12::{self, ConnectionSecrets, Tls12CipherSuite};
-use crate::verify;
+use crate::{kx, verify};
 
 use super::client_conn::ClientConnectionData;
 use super::hs::ClientContext;

rustls/src/conn.rs
@@ -1,5 +1,6 @@
 use crate::enums::ProtocolVersion;
-use crate::error::{Error, PeerMisbehaved};
+use crate::error::{Error, InvalidMessage, PeerMisbehaved};
+use crate::key;
 #[cfg(feature = "logging")]
 use crate::log::{debug, error, trace, warn};
 use crate::msgs::alert::AlertMessagePayload;
@@ -21,7 +22,6 @@ use crate::suites::{ExtractedSecrets, PartiallyExtractedSecrets};
 #[cfg(feature = "tls12")]
 use crate::tls12::ConnectionSecrets;
 use crate::vecbuf::ChunkVecBuffer;
-use crate::{key, InvalidMessage};
 #[cfg(feature = "quic")]
 use std::collections::VecDeque;
 

rustls/src/msgs/deframer.rs
@@ -4,11 +4,11 @@ use std::ops::Range;
 use super::base::Payload;
 use super::enums::ContentType;
 use super::message::PlainMessage;
-use crate::error::{Error, PeerMisbehaved};
+use crate::enums::ProtocolVersion;
+use crate::error::{Error, InvalidMessage, PeerMisbehaved};
 use crate::msgs::codec;
 use crate::msgs::message::{MessageError, OpaqueMessage};
 use crate::record_layer::{Decrypted, RecordLayer};
-use crate::{InvalidMessage, ProtocolVersion};
 
 /// This deframer works to reconstruct TLS messages from a stream of arbitrary-sized reads.
 ///

rustls/src/msgs/message.rs
@@ -1,12 +1,11 @@
 use crate::enums::ProtocolVersion;
-use crate::error::Error;
+use crate::error::{Error, InvalidMessage};
 use crate::msgs::alert::AlertMessagePayload;
 use crate::msgs::base::Payload;
 use crate::msgs::ccs::ChangeCipherSpecPayload;
 use crate::msgs::codec::{Codec, Reader};
 use crate::msgs::enums::{AlertDescription, AlertLevel, ContentType, HandshakeType};
 use crate::msgs::handshake::HandshakeMessagePayload;
-use crate::InvalidMessage;
 
 #[derive(Debug)]
 pub enum MessagePayload {

rustls/src/tls12/mod.rs
@@ -1,14 +1,14 @@
 use crate::cipher::{MessageDecrypter, MessageEncrypter};
 use crate::conn::{CommonState, ConnectionRandoms, Side};
 use crate::enums::{CipherSuite, SignatureScheme};
+use crate::error::{Error, InvalidMessage};
+use crate::kx;
 use crate::msgs::codec::{Codec, Reader};
 use crate::msgs::enums::AlertDescription;
 use crate::msgs::handshake::KeyExchangeAlgorithm;
 use crate::suites::{BulkAlgorithm, CipherSuiteCommon, SupportedCipherSuite};
 #[cfg(feature = "secret_extraction")]
 use crate::suites::{ConnectionTrafficSecrets, PartiallyExtractedSecrets};
-use crate::Error;
-use crate::{kx, InvalidMessage};
 
 use ring::aead;
 use ring::digest::Digest;