Herrgrim0/tcpls-basics

Improve error reporting for corrupt messages

examples/tests/badssl.rs
@@ -101,7 +101,7 @@ mod online {
     fn too_many_sans() {
         connect("10000-sans.badssl.com")
             .fails()
-            .expect(r"TLS error: CorruptMessagePayload\(Handshake\)")
+            .expect(r"TLS error: CorruptMessage\(HandshakePayloadTooLarge\)")
             .go()
             .unwrap();
     }

rustls/src/tls12/mod.rs
@@ -1,14 +1,14 @@
 use crate::cipher::{MessageDecrypter, MessageEncrypter};
 use crate::conn::{CommonState, ConnectionRandoms, Side};
 use crate::enums::{CipherSuite, SignatureScheme};
-use crate::kx;
 use crate::msgs::codec::{Codec, Reader};
-use crate::msgs::enums::{AlertDescription, ContentType};
+use crate::msgs::enums::AlertDescription;
 use crate::msgs::handshake::KeyExchangeAlgorithm;
 use crate::suites::{BulkAlgorithm, CipherSuiteCommon, SupportedCipherSuite};
 #[cfg(feature = "secret_extraction")]
 use crate::suites::{ConnectionTrafficSecrets, PartiallyExtractedSecrets};
 use crate::Error;
+use crate::{kx, InvalidMessage};
 
 use ring::aead;
 use ring::digest::Digest;
@@ -500,7 +500,7 @@ pub(crate) fn decode_ecdh_params<T: Codec>(
 ) -> Result<T, Error> {
     decode_ecdh_params_::<T>(kx_params).ok_or_else(|| {
         common.send_fatal_alert(AlertDescription::DecodeError);
-        Error::CorruptMessagePayload(ContentType::Handshake)
+        Error::CorruptMessage(InvalidMessage::InvalidDhParams)
     })
 }
 

rustls/tests/client_cert_verifier.rs
@@ -13,11 +13,9 @@ use crate::common::{
 use rustls::client::WebPkiVerifier;
 use rustls::internal::msgs::base::PayloadU16;
 use rustls::server::{ClientCertVerified, ClientCertVerifier};
-use rustls::AlertDescription;
-use rustls::ContentType;
 use rustls::{
-    Certificate, ClientConnection, DistinguishedNames, Error, ServerConfig, ServerConnection,
-    SignatureScheme,
+    AlertDescription, Certificate, ClientConnection, ContentType, DistinguishedNames, Error,
+    InvalidMessage, ServerConfig, ServerConnection, SignatureScheme,
 };
 use std::sync::Arc;
 
@@ -104,8 +102,8 @@ fn client_verifier_no_schemes() {
             let err = do_handshake_until_error(&mut client, &mut server);
             assert_debug_eq(
                 err,
-                Err(ErrorFromPeer::Client(Error::CorruptMessagePayload(
-                    ContentType::Handshake,
+                Err(ErrorFromPeer::Client(Error::CorruptMessage(
+                    InvalidMessage::MissingPayload(ContentType::Handshake),
                 ))),
             );
         }

rustls/tests/server_cert_verifier.rs
@@ -11,8 +11,7 @@ use rustls::client::{
     HandshakeSignatureValid, ServerCertVerified, ServerCertVerifier, WebPkiVerifier,
 };
 use rustls::internal::msgs::handshake::DigitallySignedStruct;
-use rustls::AlertDescription;
-use rustls::{Certificate, Error, SignatureScheme};
+use rustls::{AlertDescription, Certificate, Error, InvalidMessage, SignatureScheme};
 use std::sync::Arc;
 
 #[test]
@@ -39,7 +38,7 @@ fn client_can_override_certificate_verification() {
 fn client_can_override_certificate_verification_and_reject_certificate() {
     for kt in ALL_KEY_TYPES.iter() {
         let verifier = Arc::new(MockServerVerifier::rejects_certificate(
-            Error::CorruptMessage,
+            Error::CorruptMessage(InvalidMessage::HandshakePayloadTooLarge),
         ));
 
         let server_config = Arc::new(make_server_config(*kt));
@@ -56,7 +55,9 @@ fn client_can_override_certificate_verification_and_reject_certificate() {
             assert_debug_eq(
                 errs,
                 Err(vec![
-                    ErrorFromPeer::Client(Error::CorruptMessage),
+                    ErrorFromPeer::Client(Error::CorruptMessage(
+                        InvalidMessage::HandshakePayloadTooLarge,
+                    )),
                     ErrorFromPeer::Server(Error::AlertReceived(AlertDescription::BadCertificate)),
                 ]),
             );
@@ -70,7 +71,7 @@ fn client_can_override_certificate_verification_and_reject_tls12_signatures() {
     for kt in ALL_KEY_TYPES.iter() {
         let mut client_config = make_client_config_with_versions(*kt, &[&rustls::version::TLS12]);
         let verifier = Arc::new(MockServerVerifier::rejects_tls12_signatures(
-            Error::CorruptMessage,
+            Error::CorruptMessage(InvalidMessage::HandshakePayloadTooLarge),
         ));
 
         client_config
@@ -85,7 +86,9 @@ fn client_can_override_certificate_verification_and_reject_tls12_signatures() {
         assert_debug_eq(
             errs,
             Err(vec![
-                ErrorFromPeer::Client(Error::CorruptMessage),
+                ErrorFromPeer::Client(Error::CorruptMessage(
+                    InvalidMessage::HandshakePayloadTooLarge,
+                )),
                 ErrorFromPeer::Server(Error::AlertReceived(AlertDescription::BadCertificate)),
             ]),
         );
@@ -97,7 +100,7 @@ fn client_can_override_certificate_verification_and_reject_tls13_signatures() {
     for kt in ALL_KEY_TYPES.iter() {
         let mut client_config = make_client_config_with_versions(*kt, &[&rustls::version::TLS13]);
         let verifier = Arc::new(MockServerVerifier::rejects_tls13_signatures(
-            Error::CorruptMessage,
+            Error::CorruptMessage(InvalidMessage::HandshakePayloadTooLarge),
         ));
 
         client_config
@@ -112,7 +115,9 @@ fn client_can_override_certificate_verification_and_reject_tls13_signatures() {
         assert_debug_eq(
             errs,
             Err(vec![
-                ErrorFromPeer::Client(Error::CorruptMessage),
+                ErrorFromPeer::Client(Error::CorruptMessage(
+                    InvalidMessage::HandshakePayloadTooLarge,
+                )),
                 ErrorFromPeer::Server(Error::AlertReceived(AlertDescription::BadCertificate)),
             ]),
         );

rustls/rustls

examples/tests/badssl.rs
@@ -101,7 +101,7 @@ mod online {
     fn too_many_sans() {
         connect("10000-sans.badssl.com")
             .fails()
-            .expect(r"TLS error: CorruptMessagePayload\(Handshake\)")
+            .expect(r"TLS error: InvalidMessage\(HandshakePayloadTooLarge\)")
             .go()
             .unwrap();
     }

rustls/src/tls12/mod.rs
@@ -1,14 +1,14 @@
 use crate::cipher::{MessageDecrypter, MessageEncrypter};
 use crate::conn::{CommonState, ConnectionRandoms, Side};
 use crate::enums::{CipherSuite, SignatureScheme};
-use crate::kx;
 use crate::msgs::codec::{Codec, Reader};
-use crate::msgs::enums::{AlertDescription, ContentType};
+use crate::msgs::enums::AlertDescription;
 use crate::msgs::handshake::KeyExchangeAlgorithm;
 use crate::suites::{BulkAlgorithm, CipherSuiteCommon, SupportedCipherSuite};
 #[cfg(feature = "secret_extraction")]
 use crate::suites::{ConnectionTrafficSecrets, PartiallyExtractedSecrets};
 use crate::Error;
+use crate::{kx, InvalidMessage};
 
 use ring::aead;
 use ring::digest::Digest;
@@ -500,7 +500,7 @@ pub(crate) fn decode_ecdh_params<T: Codec>(
 ) -> Result<T, Error> {
     decode_ecdh_params_::<T>(kx_params).ok_or_else(|| {
         common.send_fatal_alert(AlertDescription::DecodeError);
-        Error::CorruptMessagePayload(ContentType::Handshake)
+        Error::InvalidMessage(InvalidMessage::InvalidDhParams)
     })
 }
 

rustls/tests/client_cert_verifier.rs
@@ -13,11 +13,9 @@ use crate::common::{
 use rustls::client::WebPkiVerifier;
 use rustls::internal::msgs::base::PayloadU16;
 use rustls::server::{ClientCertVerified, ClientCertVerifier};
-use rustls::AlertDescription;
-use rustls::ContentType;
 use rustls::{
-    Certificate, ClientConnection, DistinguishedNames, Error, ServerConfig, ServerConnection,
-    SignatureScheme,
+    AlertDescription, Certificate, ClientConnection, ContentType, DistinguishedNames, Error,
+    InvalidMessage, ServerConfig, ServerConnection, SignatureScheme,
 };
 use std::sync::Arc;
 
@@ -104,8 +102,8 @@ fn client_verifier_no_schemes() {
             let err = do_handshake_until_error(&mut client, &mut server);
             assert_debug_eq(
                 err,
-                Err(ErrorFromPeer::Client(Error::CorruptMessagePayload(
-                    ContentType::Handshake,
+                Err(ErrorFromPeer::Client(Error::InvalidMessage(
+                    InvalidMessage::MissingPayload(ContentType::Handshake),
                 ))),
             );
         }

rustls/tests/server_cert_verifier.rs
@@ -11,8 +11,7 @@ use rustls::client::{
     HandshakeSignatureValid, ServerCertVerified, ServerCertVerifier, WebPkiVerifier,
 };
 use rustls::internal::msgs::handshake::DigitallySignedStruct;
-use rustls::AlertDescription;
-use rustls::{Certificate, Error, SignatureScheme};
+use rustls::{AlertDescription, Certificate, Error, InvalidMessage, SignatureScheme};
 use std::sync::Arc;
 
 #[test]
@@ -39,7 +38,7 @@ fn client_can_override_certificate_verification() {
 fn client_can_override_certificate_verification_and_reject_certificate() {
     for kt in ALL_KEY_TYPES.iter() {
         let verifier = Arc::new(MockServerVerifier::rejects_certificate(
-            Error::CorruptMessage,
+            Error::InvalidMessage(InvalidMessage::HandshakePayloadTooLarge),
         ));
 
         let server_config = Arc::new(make_server_config(*kt));
@@ -56,7 +55,9 @@ fn client_can_override_certificate_verification_and_reject_certificate() {
             assert_debug_eq(
                 errs,
                 Err(vec![
-                    ErrorFromPeer::Client(Error::CorruptMessage),
+                    ErrorFromPeer::Client(Error::InvalidMessage(
+                        InvalidMessage::HandshakePayloadTooLarge,
+                    )),
                     ErrorFromPeer::Server(Error::AlertReceived(AlertDescription::BadCertificate)),
                 ]),
             );
@@ -70,7 +71,7 @@ fn client_can_override_certificate_verification_and_reject_tls12_signatures() {
     for kt in ALL_KEY_TYPES.iter() {
         let mut client_config = make_client_config_with_versions(*kt, &[&rustls::version::TLS12]);
         let verifier = Arc::new(MockServerVerifier::rejects_tls12_signatures(
-            Error::CorruptMessage,
+            Error::InvalidMessage(InvalidMessage::HandshakePayloadTooLarge),
         ));
 
         client_config
@@ -85,7 +86,9 @@ fn client_can_override_certificate_verification_and_reject_tls12_signatures() {
         assert_debug_eq(
             errs,
             Err(vec![
-                ErrorFromPeer::Client(Error::CorruptMessage),
+                ErrorFromPeer::Client(Error::InvalidMessage(
+                    InvalidMessage::HandshakePayloadTooLarge,
+                )),
                 ErrorFromPeer::Server(Error::AlertReceived(AlertDescription::BadCertificate)),
             ]),
         );
@@ -97,7 +100,7 @@ fn client_can_override_certificate_verification_and_reject_tls13_signatures() {
     for kt in ALL_KEY_TYPES.iter() {
         let mut client_config = make_client_config_with_versions(*kt, &[&rustls::version::TLS13]);
         let verifier = Arc::new(MockServerVerifier::rejects_tls13_signatures(
-            Error::CorruptMessage,
+            Error::InvalidMessage(InvalidMessage::HandshakePayloadTooLarge),
         ));
 
         client_config
@@ -112,7 +115,9 @@ fn client_can_override_certificate_verification_and_reject_tls13_signatures() {
         assert_debug_eq(
             errs,
             Err(vec![
-                ErrorFromPeer::Client(Error::CorruptMessage),
+                ErrorFromPeer::Client(Error::InvalidMessage(
+                    InvalidMessage::HandshakePayloadTooLarge,
+                )),
                 ErrorFromPeer::Server(Error::AlertReceived(AlertDescription::BadCertificate)),
             ]),
         );