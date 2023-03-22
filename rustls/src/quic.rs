bb1232660fa6e8131aa09cf04783102e64b33218
Daniel McCarney <daniel@binaryparadox.net>
2023-03-22 09:47:00 -0400

    quic: add server_name to ServerConnection.
    After splitting up the quic server connection types consumers lost the
    ability to dig out the server name from the SNI extension the server
    received. This commit adds the `server_name` function to
    `quic::ServerConnection` to restore that ability.

--- rustls/src/quic.rs
+++ rustls/src/quic.rs
@@ -209,6 +209,25 @@ impl ServerConnection {
     pub fn reject_early_data(&mut self) {
         self.inner.core.reject_early_data()
     }
+
+    /// Retrieves the server name, if any, used to select the certificate and
+    /// private key.
+    ///
+    /// This returns `None` until some time after the client's server name indication
+    /// (SNI) extension value is processed during the handshake. It will never be
+    /// `None` when the connection is ready to send or process application data,
+    /// unless the client does not support SNI.
+    ///
+    /// This is useful for application protocols that need to enforce that the
+    /// server name matches an application layer protocol hostname. For
+    /// example, HTTP/1.1 servers commonly expect the `Host:` header field of
+    /// every request on a connection to match the hostname in the SNI extension
+    /// when the client provides the SNI extension.
+    ///
+    /// The server name is also used to match sessions during session resumption.
+    pub fn server_name(&self) -> Option<&str> {
+        self.inner.core.get_sni_str()
+    }
 }
 
 impl Deref for ServerConnection {
