a8101900ac8541526a199ee3f24f8ee4020281ee
Dirkjan Ochtman <dirkjan@ochtman.nl>
2023-03-23 11:21:44 +0100

    client: deduplicate state passed to tls13::prepare_resumption()

--- rustls/src/client/hs.rs
+++ rustls/src/client/hs.rs
@@ -297,14 +297,7 @@ fn emit_client_hello_for_retry(
                 _ => None,
             })
             .map(|resuming| {
-                tls13::prepare_resumption(
-                    &config,
-                    cx,
-                    ticket,
-                    &resuming,
-                    &mut exts,
-                    retryreq.is_some(),
-                );
+                tls13::prepare_resumption(&config, cx, &resuming, &mut exts, retryreq.is_some());
                 resuming
             })
     } else if config.enable_tickets {
