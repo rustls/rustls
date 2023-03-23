4711c2502a0ccdfe84cf6e7cb6d7ac9865159e91
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2023-03-23 15:21:55 -0700

    ClientCertVerifier: remove "abort connection" return values
    For client_auth_mandatory and client_auth_root_subjects, it was possible
    to return None to abort the connection. With the removal of the `sni`
    input parameter, this no longer makes sense, so remove the
    Option-wrapping of these return values.

--- rustls/tests/client_cert_verifier.rs
+++ rustls/tests/client_cert_verifier.rs
@@ -50,14 +50,12 @@ fn client_verifier_works() {
     for kt in ALL_KEY_TYPES.iter() {
         let client_verifier = MockClientVerifier {
             verified: ver_ok,
-            subjects: Some(
-                get_client_root_store(*kt)
-                    .roots
-                    .iter()
-                    .map(|r| DistinguishedName::from(r.subject().to_vec()))
-                    .collect(),
-            ),
-            mandatory: Some(true),
+            subjects: get_client_root_store(*kt)
+                .roots
+                .iter()
+                .map(|r| DistinguishedName::from(r.subject().to_vec()))
+                .collect(),
+            mandatory: true,
             offered_schemes: None,
         };
 
@@ -80,14 +78,12 @@ fn client_verifier_no_schemes() {
     for kt in ALL_KEY_TYPES.iter() {
         let client_verifier = MockClientVerifier {
             verified: ver_ok,
-            subjects: Some(
-                get_client_root_store(*kt)
-                    .roots
-                    .iter()
-                    .map(|r| DistinguishedName::from(r.subject().to_vec()))
-                    .collect(),
-            ),
-            mandatory: Some(true),
+            subjects: get_client_root_store(*kt)
+                .roots
+                .iter()
+                .map(|r| DistinguishedName::from(r.subject().to_vec()))
+                .collect(),
+            mandatory: true,
             offered_schemes: Some(vec![]),
         };
 
@@ -109,86 +105,18 @@ fn client_verifier_no_schemes() {
     }
 }
 
-// Common case, we do not find a root store to resolve to
-#[test]
-fn client_verifier_no_root() {
-    for kt in ALL_KEY_TYPES.iter() {
-        let client_verifier = MockClientVerifier {
-            verified: ver_ok,
-            subjects: None,
-            mandatory: Some(true),
-            offered_schemes: None,
-        };
-
-        let server_config = server_config_with_verifier(*kt, client_verifier);
-        let server_config = Arc::new(server_config);
-
-        for version in rustls::ALL_VERSIONS {
-            let client_config = make_client_config_with_versions_with_auth(*kt, &[version]);
-            let mut server = ServerConnection::new(Arc::clone(&server_config)).unwrap();
-            let mut client =
-                ClientConnection::new(Arc::new(client_config), dns_name("notlocalhost")).unwrap();
-            let errs = do_handshake_until_both_error(&mut client, &mut server);
-            assert_eq!(
-                errs,
-                Err(vec![
-                    ErrorFromPeer::Server(Error::General(
-                        "client rejected by client_auth_root_subjects".into()
-                    )),
-                    ErrorFromPeer::Client(Error::AlertReceived(AlertDescription::AccessDenied))
-                ])
-            );
-        }
-    }
-}
-
-// If we cannot resolve a root, we cannot decide if auth is mandatory
-#[test]
-fn client_verifier_no_auth_no_root() {
-    for kt in ALL_KEY_TYPES.iter() {
-        let client_verifier = MockClientVerifier {
-            verified: ver_unreachable,
-            subjects: None,
-            mandatory: Some(true),
-            offered_schemes: None,
-        };
-
-        let server_config = server_config_with_verifier(*kt, client_verifier);
-        let server_config = Arc::new(server_config);
-
-        for version in rustls::ALL_VERSIONS {
-            let client_config = make_client_config_with_versions(*kt, &[version]);
-            let mut server = ServerConnection::new(Arc::clone(&server_config)).unwrap();
-            let mut client =
-                ClientConnection::new(Arc::new(client_config), dns_name("notlocalhost")).unwrap();
-            let errs = do_handshake_until_both_error(&mut client, &mut server);
-            assert_eq!(
-                errs,
-                Err(vec![
-                    ErrorFromPeer::Server(Error::General(
-                        "client rejected by client_auth_root_subjects".into()
-                    )),
-                    ErrorFromPeer::Client(Error::AlertReceived(AlertDescription::AccessDenied))
-                ])
-            );
-        }
-    }
-}
-
 // If we do have a root, we must do auth
 #[test]
 fn client_verifier_no_auth_yes_root() {
     for kt in ALL_KEY_TYPES.iter() {
         let client_verifier = MockClientVerifier {
             verified: ver_unreachable,
-            subjects: Some(
-                get_client_root_store(*kt)
-                    .roots
-                    .iter()
-                    .map(|r| DistinguishedName::from(r.subject().to_vec()))
-                    .collect(),
-            ),
-            mandatory: Some(true),
+            subjects: get_client_root_store(*kt)
+                .roots
+                .iter()
+                .map(|r| DistinguishedName::from(r.subject().to_vec()))
+                .collect(),
+            mandatory: true,
             offered_schemes: None,
         };
 
@@ -220,14 +148,12 @@ fn client_verifier_fails_properly() {
     for kt in ALL_KEY_TYPES.iter() {
         let client_verifier = MockClientVerifier {
             verified: ver_err,
-            subjects: Some(
-                get_client_root_store(*kt)
-                    .roots
-                    .iter()
-                    .map(|r| DistinguishedName::from(r.subject().to_vec()))
-                    .collect(),
-            ),
-            mandatory: Some(true),
+            subjects: get_client_root_store(*kt)
+                .roots
+                .iter()
+                .map(|r| DistinguishedName::from(r.subject().to_vec()))
+                .collect(),
+            mandatory: true,
             offered_schemes: None,
         };
 
@@ -248,59 +174,20 @@ fn client_verifier_fails_properly() {
     }
 }
 
-#[test]
-// If a verifier returns a None on Mandatory-ness, then we error out
-fn client_verifier_must_determine_client_auth_requirement_to_continue() {
-    for kt in ALL_KEY_TYPES.iter() {
-        let client_verifier = MockClientVerifier {
-            verified: ver_ok,
-            subjects: Some(
-                get_client_root_store(*kt)
-                    .roots
-                    .iter()
-                    .map(|r| DistinguishedName::from(r.subject().to_vec()))
-                    .collect(),
-            ),
-            mandatory: None,
-            offered_schemes: None,
-        };
-
-        let server_config = server_config_with_verifier(*kt, client_verifier);
-        let server_config = Arc::new(server_config);
-
-        for version in rustls::ALL_VERSIONS {
-            let client_config = make_client_config_with_versions_with_auth(*kt, &[version]);
-            let mut server = ServerConnection::new(Arc::clone(&server_config)).unwrap();
-            let mut client =
-                ClientConnection::new(Arc::new(client_config), dns_name("localhost")).unwrap();
-            let errs = do_handshake_until_both_error(&mut client, &mut server);
-            assert_eq!(
-                errs,
-                Err(vec![
-                    ErrorFromPeer::Server(Error::General(
-                        "client rejected by client_auth_mandatory".into()
-                    )),
-                    ErrorFromPeer::Client(Error::AlertReceived(AlertDescription::AccessDenied))
-                ])
-            );
-        }
-    }
-}
-
 pub struct MockClientVerifier {
     pub verified: fn() -> Result<ClientCertVerified, Error>,
-    pub subjects: Option<Vec<DistinguishedName>>,
-    pub mandatory: Option<bool>,
+    pub subjects: Vec<DistinguishedName>,
+    pub mandatory: bool,
     pub offered_schemes: Option<Vec<SignatureScheme>>,
 }
 
 impl ClientCertVerifier for MockClientVerifier {
-    fn client_auth_mandatory(&self) -> Option<bool> {
+    fn client_auth_mandatory(&self) -> bool {
         self.mandatory
     }
 
-    fn client_auth_root_subjects(&self) -> Option<Vec<DistinguishedName>> {
-        self.subjects.as_ref().cloned()
+    fn client_auth_root_subjects(&self) -> Vec<DistinguishedName> {
+        self.subjects.clone()
     }
 
     fn verify_client_cert(
