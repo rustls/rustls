rustls/rustls

commit ae4ca3290ba256748f5b5287714a853600cf0b55

Cease relying on webpki crate for DNS name types
This introduces similar but simplified `DnsName` and
`DnsNameRef` types.

Fuzz the `client::ServerName` type that exposes this
surface.

rustls/src/dns_name.rs
@@ -1,0 +1,219 @@
+/// DNS name validation according to RFC1035, but with underscores allowed.
+use std::error::Error as StdError;
+use std::fmt;
+
+/// A type which encapsulates an owned string that is a syntactically valid DNS name.
+#[derive(Clone, Eq, Hash, PartialEq, Debug)]
+pub struct DnsName(String);
+
+impl<'a> DnsName {
+    /// Produce a borrowed `DnsNameRef` from this owned `DnsName`.
+    pub fn borrow(&'a self) -> DnsNameRef<'a> {
+        DnsNameRef(self.as_ref())
+    }
+
+    /// Validate the given bytes are a DNS name if they are viewed as ASCII.
+    pub fn try_from_ascii(bytes: &[u8]) -> Result<Self, InvalidDnsNameError> {
+        // nb. a sequence of bytes that is accepted by `validate()` is both
+        // valid UTF-8, and valid ASCII.
+        String::from_utf8(bytes.to_vec())
+            .map_err(|_| InvalidDnsNameError)
+            .and_then(Self::try_from)
+    }
+}
+
+impl TryFrom<String> for DnsName {
+    type Error = InvalidDnsNameError;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        validate(value.as_bytes())?;
+        Ok(Self(value))
+    }
+}
+
+impl AsRef<str> for DnsName {
+    fn as_ref(&self) -> &str {
+        AsRef::<str>::as_ref(&self.0)
+    }
+}
+
+/// A type which encapsulates a borrowed string that is a syntactically valid DNS name.
+#[derive(Eq, Hash, PartialEq, Debug)]
+pub struct DnsNameRef<'a>(&'a str);
+
+impl<'a> DnsNameRef<'a> {
+    /// Copy this object to produce an owned `DnsName`.
+    pub fn to_owned(&'a self) -> DnsName {
+        DnsName(self.0.to_string())
+    }
+
+    /// Copy this object to produce an owned `DnsName`, smashing the case to lowercase
+    /// in one operation.
+    pub fn to_lowercase_owned(&'a self) -> DnsName {
+        DnsName(self.0.to_lowercase())
+    }
+}
+
+impl<'a> TryFrom<&'a str> for DnsNameRef<'a> {
+    type Error = InvalidDnsNameError;
+
+    fn try_from(value: &'a str) -> Result<DnsNameRef<'a>, Self::Error> {
+        validate(value.as_bytes())?;
+        Ok(DnsNameRef(value))
+    }
+}
+
+impl<'a> AsRef<str> for DnsNameRef<'a> {
+    fn as_ref(&self) -> &str {
+        self.0
+    }
+}
+
+/// The provided input could not be parsed because
+/// it is not a syntactically-valid DNS Name.
+#[derive(Debug)]
+pub struct InvalidDnsNameError;
+
+impl fmt::Display for InvalidDnsNameError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str("invalid dns name")
+    }
+}
+
+impl StdError for InvalidDnsNameError {}
+
+fn validate(input: &[u8]) -> Result<(), InvalidDnsNameError> {
+    use State::*;
+    let mut state = Start;
+
+    /// "Labels must be 63 characters or less."
+    const MAX_LABEL_LENGTH: usize = 63;
+
+    for ch in input {
+        state = match (state, ch) {
+            (Start | Next | Hyphen { .. }, b'.') => return Err(InvalidDnsNameError),
+            (Subsequent { .. }, b'.') => Next,
+            (Subsequent { len }, _) if len >= MAX_LABEL_LENGTH => return Err(InvalidDnsNameError),
+            (Start | Next, b'a'..=b'z' | b'A'..=b'Z') => Subsequent { len: 1 },
+            (Subsequent { len }, b'-') => Hyphen { len: len + 1 },
+            (
+                Subsequent { len } | Hyphen { len },
+                b'a'..=b'z' | b'A'..=b'Z' | b'_' | b'-' | b'0'..=b'9',
+            ) => Subsequent { len: len + 1 },
+            _ => return Err(InvalidDnsNameError),
+        };
+    }
+
+    if matches!(state, Start | Hyphen { .. }) {
+        return Err(InvalidDnsNameError);
+    }
+
+    Ok(())
+}
+
+enum State {
+    Start,
+    Next,
+    Subsequent { len: usize },
+    Hyphen { len: usize },
+}
+
+#[cfg(test)]
+mod test {
+    static TESTS: &[(&'static str, bool)] = &[
+        ("", false),
+        ("localhost", true),
+        ("LOCALHOST", true),
+        (".localhost", false),
+        ("..localhost", false),
+        ("1.2.3.4", false),
+        ("127.0.0.1", false),
+        ("absolute.", true),
+        ("absolute..", false),
+        ("multiple.labels.absolute.", true),
+        ("foo.bar.com", true),
+        ("infix-hyphen-allowed.com", true),
+        ("-prefixhypheninvalid.com", false),
+        ("suffixhypheninvalid-.com", false),
+        ("foo.lastlabelendswithhyphen-", false),
+        ("infix_underscore_allowed.com", true),
+        ("_prefixunderscoreinvalid.com", false),
+        ("labelendswithnumber1.bar.com", true),
+        ("xn--bcher-kva.example", true),
+        (
+            "sixtythreesixtythreesixtythreesixtythreesixtythreesixtythreesix.com",
+            true,
+        ),
+        (
+            "sixtyfoursixtyfoursixtyfoursixtyfoursixtyfoursixtyfoursixtyfours.com",
+            false,
+        ),
+    ];
+
+    #[test]
+    fn test_validation() {
+        for (input, expected) in TESTS {
+            println!("test: {:?} expected valid? {:?}", input, expected);
+            let name_ref = super::DnsNameRef::try_from(*input);
+            assert_eq!(*expected, name_ref.is_ok());
+            let name = super::DnsName::try_from(input.to_string());
+            assert_eq!(*expected, name.is_ok());
+        }
+    }
+
+    #[test]
+    fn error_is_debug() {
+        assert_eq!(
+            format!("{:?}", super::InvalidDnsNameError),
+            "InvalidDnsNameError"
+        );
+    }
+
+    #[test]
+    fn error_is_display() {
+        assert_eq!(
+            format!("{}", super::InvalidDnsNameError),
+            "invalid dns name"
+        );
+    }
+
+    #[test]
+    fn dns_name_is_debug() {
+        let example = super::DnsName::try_from("example.com".to_string()).unwrap();
+        assert_eq!(format!("{:?}", example), "DnsName(\"example.com\")");
+    }
+
+    #[test]
+    fn dns_name_traits() {
+        let example = super::DnsName::try_from("example.com".to_string()).unwrap();
+        assert_eq!(example, example); // PartialEq
+
+        use std::collections::HashSet;
+        let mut h = HashSet::<super::DnsName>::new();
+        h.insert(example);
+    }
+
+    #[test]
+    fn try_from_ascii_rejects_bad_utf8() {
+        assert_eq!(
+            format!("{:?}", super::DnsName::try_from_ascii(b"\x80")),
+            "Err(InvalidDnsNameError)"
+        );
+    }
+
+    #[test]
+    fn dns_name_ref_is_debug() {
+        let example = super::DnsNameRef::try_from("example.com").unwrap();
+        assert_eq!(format!("{:?}", example), "DnsNameRef(\"example.com\")");
+    }
+
+    #[test]
+    fn dns_name_ref_traits() {
+        let example = super::DnsNameRef::try_from("example.com").unwrap();
+        assert_eq!(example, example); // PartialEq
+
+        use std::collections::HashSet;
+        let mut h = HashSet::<super::DnsNameRef>::new();
+        h.insert(example);
+    }
+}

fuzz/Cargo.toml
@@ -44,3 +44,7 @@ path = "fuzzers/client.rs"
 [[bin]]
 name = "server"
 path = "fuzzers/server.rs"
+
+[[bin]]
+name = "server_name"
+path = "fuzzers/server_name.rs"

fuzz/fuzzers/server_name.rs
@@ -1,0 +1,12 @@
+#![no_main]
+#[macro_use]
+extern crate libfuzzer_sys;
+extern crate rustls;
+
+use rustls::client::ServerName;
+
+fuzz_target!(|data: &[u8]| {
+    let _ = std::str::from_utf8(data)
+        .map_err(|_| ())
+        .and_then(|s| ServerName::try_from(s).map_err(|_| ()));
+});

rustls/src/client/client_conn.rs
@@ -1,6 +1,7 @@
 use crate::builder::{ConfigBuilder, WantsCipherSuites};
 use crate::common_state::{CommonState, Protocol, Side};
 use crate::conn::{ConnectionCommon, ConnectionCore};
+use crate::dns_name;
 use crate::enums::{CipherSuite, ProtocolVersion, SignatureScheme};
 use crate::error::Error;
 use crate::kx::SupportedKxGroup;
@@ -20,7 +21,6 @@ use crate::KeyLog;
 use super::handy::{ClientSessionMemoryCache, NoClientSessionStorage};
 use super::hs;
 
-use std::error::Error as StdError;
 use std::marker::PhantomData;
 use std::net::IpAddr;
 use std::ops::{Deref, DerefMut};
@@ -334,25 +334,40 @@ impl Default for Resumption {
 /// # let _: ServerName = x;
 /// ```
 #[non_exhaustive]
-#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+#[derive(Clone, Eq, Hash, PartialEq)]
 pub enum ServerName {
     /// The server is identified by a DNS name.  The name
     /// is sent in the TLS Server Name Indication (SNI)
     /// extension.
-    DnsName(verify::DnsName),
+    DnsName(dns_name::DnsName),
 
     /// The server is identified by an IP address. SNI is not
     /// done.
     IpAddress(IpAddr),
 }
 
+impl fmt::Debug for ServerName {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::DnsName(d) => f
+                .debug_tuple("DnsName")
+                .field(&d.as_ref())
+                .finish(),
+            Self::IpAddress(i) => f
+                .debug_tuple("IpAddress")
+                .field(i)
+                .finish(),
+        }
+    }
+}
+
 impl ServerName {
     /// Return the name that should go in the SNI extension.
     /// If [`None`] is returned, the SNI extension is not included
     /// in the handshake.
-    pub(crate) fn for_sni(&self) -> Option<webpki::DnsNameRef> {
+    pub(crate) fn for_sni(&self) -> Option<dns_name::DnsNameRef> {
         match self {
-            Self::DnsName(dns_name) => Some(dns_name.0.as_ref()),
+            Self::DnsName(dns_name) => Some(dns_name.borrow()),
             Self::IpAddress(_) => None,
         }
     }
@@ -361,30 +376,17 @@ impl ServerName {
 /// Attempt to make a ServerName from a string by parsing
 /// it as a DNS name.
 impl TryFrom<&str> for ServerName {
-    type Error = InvalidDnsNameError;
+    type Error = dns_name::InvalidDnsNameError;
     fn try_from(s: &str) -> Result<Self, Self::Error> {
-        match webpki::DnsNameRef::try_from_ascii_str(s) {
-            Ok(dns) => Ok(Self::DnsName(verify::DnsName(dns.into()))),
-            Err(webpki::InvalidDnsNameError) => match s.parse() {
+        match dns_name::DnsNameRef::try_from(s) {
+            Ok(dns) => Ok(Self::DnsName(dns.to_owned())),
+            Err(dns_name::InvalidDnsNameError) => match s.parse() {
                 Ok(ip) => Ok(Self::IpAddress(ip)),
-                Err(_) => Err(InvalidDnsNameError),
+                Err(_) => Err(dns_name::InvalidDnsNameError),
             },
         }
     }
 }
-
-/// The provided input could not be parsed because
-/// it is not a syntactically-valid DNS Name.
-#[derive(Debug)]
-pub struct InvalidDnsNameError;
-
-impl fmt::Display for InvalidDnsNameError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        f.write_str("invalid dns name")
-    }
-}
-
-impl StdError for InvalidDnsNameError {}
 
 /// Container for unsafe APIs
 #[cfg(feature = "dangerous_configuration")]

rustls/src/lib.rs
@@ -318,6 +318,7 @@ mod anchors;
 mod cipher;
 mod common_state;
 mod conn;
+mod dns_name;
 mod error;
 mod hash_hs;
 mod limited_cache;
@@ -405,11 +406,11 @@ pub mod client {
     mod tls12;
     mod tls13;
 
+    pub use crate::dns_name::InvalidDnsNameError;
     pub use builder::{WantsClientCert, WantsTransparencyPolicyOrClientCert};
     pub use client_conn::{
         ClientConfig, ClientConnection, ClientConnectionData, ClientSessionStore,
-        InvalidDnsNameError, ResolvesClientCert, Resumption, ServerName, Tls12Resumption,
-        WriteEarlyData,
+        ResolvesClientCert, Resumption, ServerName, Tls12Resumption, WriteEarlyData,
     };
     pub use handy::ClientSessionMemoryCache;
 
@@ -451,7 +452,9 @@ pub mod server {
     pub use server_conn::{ClientHello, ProducesTickets, ResolvesServerCert};
 
     #[cfg(feature = "dangerous_configuration")]
-    pub use crate::verify::{ClientCertVerified, ClientCertVerifier, DnsName};
+    pub use crate::dns_name::DnsName;
+    #[cfg(feature = "dangerous_configuration")]
+    pub use crate::verify::{ClientCertVerified, ClientCertVerifier};
 }
 
 pub use server::{ServerConfig, ServerConnection};

rustls/src/msgs/handshake.rs
@@ -1,4 +1,5 @@
 #![allow(non_camel_case_types)]
+use crate::dns_name::{DnsName, DnsNameRef};
 use crate::enums::{CipherSuite, HandshakeType, ProtocolVersion, SignatureScheme};
 use crate::error::InvalidMessage;
 use crate::key;
@@ -209,41 +210,36 @@ impl TlsListElement for SignatureScheme {
 
 #[derive(Clone, Debug)]
 pub enum ServerNamePayload {
-    // Stored twice, bytes so we can round-trip, and DnsName for use
-    HostName((PayloadU16, webpki::DnsName)),
+    HostName(DnsName),
     Unknown(Payload),
 }
 
 impl ServerNamePayload {
-    pub fn new_hostname(hostname: webpki::DnsName) -> Self {
-        let raw = {
-            let s: &str = hostname.as_ref().into();
-            PayloadU16::new(s.as_bytes().into())
-        };
-        Self::HostName((raw, hostname))
+    pub fn new_hostname(hostname: DnsName) -> Self {
+        Self::HostName(hostname)
     }
 
     fn read_hostname(r: &mut Reader) -> Result<Self, InvalidMessage> {
         let raw = PayloadU16::read(r)?;
-        let dns_name = {
-            match webpki::DnsNameRef::try_from_ascii(&raw.0) {
-                Ok(dns_name) => dns_name.into(),
-                Err(_) => {
-                    warn!(
-                        "Illegal SNI hostname received {:?}",
-                        String::from_utf8_lossy(&raw.0)
-                    );
-                    return Err(InvalidMessage::InvalidServerName);
-                }
-            }
-        };
 
-        Ok(Self::HostName((raw, dns_name)))
+        match DnsName::try_from_ascii(&raw.0) {
+            Ok(dns_name) => Ok(Self::HostName(dns_name)),
+            Err(_) => {
+                warn!(
+                    "Illegal SNI hostname received {:?}",
+                    String::from_utf8_lossy(&raw.0)
+                );
+                Err(InvalidMessage::InvalidServerName)
+            }
+        }
     }
 
     fn encode(&self, bytes: &mut Vec<u8>) {
         match *self {
-            Self::HostName((ref r, _)) => r.encode(bytes),
+            Self::HostName(ref name) => {
+                (name.as_ref().len() as u16).encode(bytes);
+                bytes.extend_from_slice(name.as_ref().as_bytes());
+            }
             Self::Unknown(ref r) => r.encode(bytes),
         }
     }
@@ -279,7 +275,7 @@ impl TlsListElement for ServerName {
 
 pub trait ConvertServerNameList {
     fn has_duplicate_names_for_type(&self) -> bool;
-    fn get_single_hostname(&self) -> Option<webpki::DnsNameRef>;
+    fn get_single_hostname(&self) -> Option<DnsNameRef>;
 }
 
 impl ConvertServerNameList for [ServerName] {
@@ -296,10 +292,10 @@ impl ConvertServerNameList for [ServerName] {
         false
     }
 
-    fn get_single_hostname(&self) -> Option<webpki::DnsNameRef> {
-        fn only_dns_hostnames(name: &ServerName) -> Option<webpki::DnsNameRef> {
-            if let ServerNamePayload::HostName((_, ref dns)) = name.payload {
-                Some(dns.as_ref())
+    fn get_single_hostname(&self) -> Option<DnsNameRef> {
+        fn only_dns_hostnames(name: &ServerName) -> Option<DnsNameRef> {
+            if let ServerNamePayload::HostName(ref dns) = name.payload {
+                Some(dns.borrow())
             } else {
                 None
             }
@@ -667,14 +663,14 @@ impl Codec for ClientExtension {
     }
 }
 
-fn trim_hostname_trailing_dot_for_sni(dns_name: webpki::DnsNameRef) -> webpki::DnsName {
-    let dns_name_str: &str = dns_name.into();
+fn trim_hostname_trailing_dot_for_sni(dns_name: DnsNameRef) -> DnsName {
+    let dns_name_str: &str = dns_name.as_ref();
 
     // RFC6066: "The hostname is represented as a byte string using
     // ASCII encoding without a trailing dot"
     if dns_name_str.ends_with('.') {
         let trimmed = &dns_name_str[0..dns_name_str.len() - 1];
-        webpki::DnsNameRef::try_from_ascii_str(trimmed)
+        DnsNameRef::try_from(trimmed)
             .unwrap()
             .to_owned()
     } else {
@@ -684,7 +680,7 @@ fn trim_hostname_trailing_dot_for_sni(dns_name: webpki::DnsNameRef) -> webpki::D
 
 impl ClientExtension {
     /// Make a basic SNI ServerNameRequest quoting `hostname`.
-    pub fn make_sni(dns_name: webpki::DnsNameRef) -> Self {
+    pub fn make_sni(dns_name: DnsNameRef) -> Self {
         let name = ServerName {
             typ: ServerNameType::HostName,
             payload: ServerNamePayload::new_hostname(trim_hostname_trailing_dot_for_sni(dns_name)),

rustls/src/msgs/handshake_test.rs
@@ -1,3 +1,4 @@
+use crate::dns_name::DnsNameRef;
 use crate::enums::{CipherSuite, HandshakeType, ProtocolVersion, SignatureScheme};
 use crate::key::Certificate;
 use crate::msgs::base::{Payload, PayloadU16, PayloadU24, PayloadU8};
@@ -18,8 +19,6 @@ use crate::msgs::handshake::{
     ServerExtension, ServerHelloPayload, ServerKeyExchangePayload, SessionId, UnknownExtension,
 };
 use crate::verify::DigitallySignedStruct;
-
-use webpki::DnsNameRef;
 
 #[test]
 fn rejects_short_random() {
@@ -222,11 +221,8 @@ fn can_roundtrip_multiname_sni() {
 
             assert!(req.has_duplicate_names_for_type());
 
-            let dns_name_str: &str = req
-                .get_single_hostname()
-                .unwrap()
-                .into();
-            assert_eq!(dns_name_str, "hi");
+            let dns_name = req.get_single_hostname().unwrap();
+            assert_eq!(dns_name.as_ref(), "hi");
 
             assert_eq!(req[0].typ, ServerNameType::HostName);
             assert_eq!(req[1].typ, ServerNameType::HostName);
@@ -369,7 +365,7 @@ fn get_sample_clienthellopayload() -> ClientHelloPayload {
             ClientExtension::ECPointFormats(ECPointFormat::SUPPORTED.to_vec()),
             ClientExtension::NamedGroups(vec![NamedGroup::X25519]),
             ClientExtension::SignatureAlgorithms(vec![SignatureScheme::ECDSA_NISTP256_SHA256]),
-            ClientExtension::make_sni(DnsNameRef::try_from_ascii_str("hello").unwrap()),
+            ClientExtension::make_sni(DnsNameRef::try_from("hello").unwrap()),
             ClientExtension::SessionTicket(ClientSessionTicket::Request),
             ClientExtension::SessionTicket(ClientSessionTicket::Offer(Payload(vec![]))),
             ClientExtension::Protocols(vec![ProtocolName::from(vec![0])]),

rustls/src/msgs/persist.rs
@@ -1,3 +1,4 @@
+use crate::dns_name;
 use crate::enums::{CipherSuite, ProtocolVersion};
 use crate::error::InvalidMessage;
 use crate::key;
@@ -254,7 +255,7 @@ pub type ServerSessionKey = SessionId;
 
 #[derive(Debug)]
 pub struct ServerSessionValue {
-    pub sni: Option<webpki::DnsName>,
+    pub sni: Option<dns_name::DnsName>,
     pub version: ProtocolVersion,
     pub cipher_suite: CipherSuite,
     pub master_secret: PayloadU8,
@@ -271,7 +272,7 @@ impl Codec for ServerSessionValue {
     fn encode(&self, bytes: &mut Vec<u8>) {
         if let Some(ref sni) = self.sni {
             1u8.encode(bytes);
-            let sni_bytes: &str = sni.as_ref().into();
+            let sni_bytes: &str = sni.as_ref();
             PayloadU8::new(Vec::from(sni_bytes)).encode(bytes);
         } else {
             0u8.encode(bytes);
@@ -302,12 +303,12 @@ impl Codec for ServerSessionValue {
         let has_sni = u8::read(r)?;
         let sni = if has_sni == 1 {
             let dns_name = PayloadU8::read(r)?;
-            let dns_name = match webpki::DnsNameRef::try_from_ascii(&dns_name.0) {
+            let dns_name = match dns_name::DnsName::try_from_ascii(&dns_name.0) {
                 Ok(dns_name) => dns_name,
                 Err(_) => return Err(InvalidMessage::InvalidServerName),
             };
 
-            Some(dns_name.into())
+            Some(dns_name)
         } else {
             None
         };
@@ -350,7 +351,7 @@ impl Codec for ServerSessionValue {
 
 impl ServerSessionValue {
     pub fn new(
-        sni: Option<&webpki::DnsName>,
+        sni: Option<&dns_name::DnsName>,
         v: ProtocolVersion,
         cs: CipherSuite,
         ms: Vec<u8>,

commit 86cac4f891b3b4d90b01679ae2ebe8429fb42fc5

Fix connect-tests for 1000-sans.badssl.com
This regressed in ae4ca32.  DNS names starting with
and wholly numerals are in wide use.

RFC1035 actually doesn't really reflect reality, and RFC1123
updates it to allow labels that start with numerals or are
wholly numeric.  The only restriction is that the last label
cannot be wholly numeric.

Also:

- Limit the entire length of a DNS name.
- Ensure a label that ends with hyphens respects the label length limit.

rustls/src/dns_name.rs
@@ -89,22 +89,45 @@ fn validate(input: &[u8]) -> Result<(), InvalidDnsNameError> {
     /// "Labels must be 63 characters or less."
     const MAX_LABEL_LENGTH: usize = 63;
 
+    /// https://devblogs.microsoft.com/oldnewthing/20120412-00/?p=7873
+    const MAX_NAME_LENGTH: usize = 253;
+
+    if input.len() > MAX_NAME_LENGTH {
+        return Err(InvalidDnsNameError);
+    }
+
     for ch in input {
         state = match (state, ch) {
-            (Start | Next | Hyphen { .. }, b'.') => return Err(InvalidDnsNameError),
+            (Start | Next | NextAfterNumericOnly | Hyphen { .. }, b'.') => {
+                return Err(InvalidDnsNameError)
+            }
             (Subsequent { .. }, b'.') => Next,
-            (Subsequent { len }, _) if len >= MAX_LABEL_LENGTH => return Err(InvalidDnsNameError),
-            (Start | Next, b'a'..=b'z' | b'A'..=b'Z') => Subsequent { len: 1 },
-            (Subsequent { len }, b'-') => Hyphen { len: len + 1 },
+            (NumericOnly { .. }, b'.') => NextAfterNumericOnly,
+            (Subsequent { len } | NumericOnly { len } | Hyphen { len }, _)
+                if len >= MAX_LABEL_LENGTH =>
+            {
+                return Err(InvalidDnsNameError)
+            }
+            (Start | Next | NextAfterNumericOnly, b'0'..=b'9') => NumericOnly { len: 1 },
+            (NumericOnly { len }, b'0'..=b'9') => NumericOnly { len: len + 1 },
+            (Start | Next | NextAfterNumericOnly, b'a'..=b'z' | b'A'..=b'Z' | b'_') => {
+                Subsequent { len: 1 }
+            }
+            (Subsequent { len } | NumericOnly { len } | Hyphen { len }, b'-') => {
+                Hyphen { len: len + 1 }
+            }
             (
-                Subsequent { len } | Hyphen { len },
-                b'a'..=b'z' | b'A'..=b'Z' | b'_' | b'-' | b'0'..=b'9',
+                Subsequent { len } | NumericOnly { len } | Hyphen { len },
+                b'a'..=b'z' | b'A'..=b'Z' | b'_' | b'0'..=b'9',
             ) => Subsequent { len: len + 1 },
             _ => return Err(InvalidDnsNameError),
         };
     }
 
-    if matches!(state, Start | Hyphen { .. }) {
+    if matches!(
+        state,
+        Start | Hyphen { .. } | NumericOnly { .. } | NextAfterNumericOnly
+    ) {
         return Err(InvalidDnsNameError);
     }
 
@@ -114,6 +137,8 @@ fn validate(input: &[u8]) -> Result<(), InvalidDnsNameError> {
 enum State {
     Start,
     Next,
+    NumericOnly { len: usize },
+    NextAfterNumericOnly,
     Subsequent { len: usize },
     Hyphen { len: usize },
 }
@@ -134,10 +159,11 @@ mod test {
         ("foo.bar.com", true),
         ("infix-hyphen-allowed.com", true),
         ("-prefixhypheninvalid.com", false),
+        ("suffixhypheninvalid--", false),
         ("suffixhypheninvalid-.com", false),
         ("foo.lastlabelendswithhyphen-", false),
         ("infix_underscore_allowed.com", true),
-        ("_prefixunderscoreinvalid.com", false),
+        ("_prefixunderscorevalid.com", true),
         ("labelendswithnumber1.bar.com", true),
         ("xn--bcher-kva.example", true),
         (
@@ -148,6 +174,33 @@ mod test {
             "sixtyfoursixtyfoursixtyfoursixtyfoursixtyfoursixtyfoursixtyfours.com",
             false,
         ),
+        (
+            "012345678901234567890123456789012345678901234567890123456789012.com",
+            true,
+        ),
+        (
+            "0123456789012345678901234567890123456789012345678901234567890123.com",
+            false,
+        ),
+        (
+            "01234567890123456789012345678901234567890123456789012345678901-.com",
+            false,
+        ),
+        (
+            "012345678901234567890123456789012345678901234567890123456789012-.com",
+            false,
+        ),
+        ("numeric-only-final-label.1", false),
+        ("numeric-only-final-label.absolute.1.", false),
+        ("1starts-with-number.com", true),
+        ("1Starts-with-number.com", true),
+        ("1.2.3.4.com", true),
+        ("123.numeric-only-first-label", true),
+        ("a123b.com", true),
+        ("numeric-only-middle-label.4.com", true),
+        ("1000-sans.badssl.com", true),
+        ("twohundredandfiftythreecharacters.twohundredandfiftythreecharacters.twohundredandfiftythreecharacters.twohundredandfiftythreecharacters.twohundredandfiftythreecharacters.twohundredandfiftythreecharacters.twohundredandfiftythreecharacters.twohundredandfi", true),
+        ("twohundredandfiftyfourcharacters.twohundredandfiftyfourcharacters.twohundredandfiftyfourcharacters.twohundredandfiftyfourcharacters.twohundredandfiftyfourcharacters.twohundredandfiftyfourcharacters.twohundredandfiftyfourcharacters.twohundredandfiftyfourc", false),
     ];
 
     #[test]