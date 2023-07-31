d4535756cf6cf5a87583fc02a5ef8bb00f8fea16
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2023-07-31 18:31:21 +0200

    enable clippy lints to prevent future uses of re-exported std API

--- rustls/src/lib.rs
+++ rustls/src/lib.rs
@@ -261,7 +261,9 @@
 #![forbid(unsafe_code, unused_must_use)]
 #![cfg_attr(not(any(read_buf, bench)), forbid(unstable_features))]
 #![deny(
+    clippy::alloc_instead_of_core,
     clippy::clone_on_ref_ptr,
+    clippy::std_instead_of_core,
     clippy::use_self,
     trivial_casts,
     trivial_numeric_casts,
