a2475a4c9f6eb4d11da4176eeed96841dc7e072f
Dirkjan Ochtman <dirkjan@ochtman.nl>
2023-08-02 10:03:13 +0200

    Fix up nightly clippy issue with incorrect comment

--- rustls/src/server/server_conn.rs
+++ rustls/src/server/server_conn.rs
@@ -562,7 +562,7 @@ impl From<ServerConnection> for crate::Connection {
 ///     // Proceed with handling the ServerConnection.
 /// }
 /// # }
-//// ```
+/// ```
 pub struct Acceptor {
     inner: Option<ConnectionCommon<ServerConnectionData>>,
 }
