8cf2594dcb0fa523a1c62dbca279e3d4a41b8372
Daniel McCarney <daniel@binaryparadox.net>
2023-10-24 09:23:17 -0400

    sign: fix clippy get-first warning

--- rustls/src/sign.rs
+++ rustls/src/sign.rs
@@ -68,7 +68,7 @@ impl CertifiedKey {
 
     /// The end-entity certificate.
     pub fn end_entity_cert(&self) -> Result<&key::Certificate, SignError> {
-        self.cert.get(0).ok_or(SignError(()))
+        self.cert.first().ok_or(SignError(()))
     }
 }
 
