2f193a1f7b43307d7075219cb21226e2f2937ecb
Daniel McCarney <daniel@binaryparadox.net>
2023-10-26 10:45:39 -0400

    webpki: re-order RootCertStore members
    This commit re-orders the `RootCertStore` members based on
    CONTRIBUTING.md style guide. Mainly:
    
    1. Associated fn (without `self` arg) first.
    2. The rest ordered by code complexity.

--- rustls/src/webpki/anchors.rs
+++ rustls/src/webpki/anchors.rs
@@ -22,34 +22,6 @@ impl RootCertStore {
         Self { roots: Vec::new() }
     }
 
-    /// Return true if there are no certificates.
-    pub fn is_empty(&self) -> bool {
-        self.len() == 0
-    }
-
-    /// Say how many certificates are in the container.
-    pub fn len(&self) -> usize {
-        self.roots.len()
-    }
-
-    /// Add a single DER-encoded certificate to the store.
-    ///
-    /// This is suitable for a small set of root certificates that are expected to parse
-    /// successfully. For large collections of roots (for example from a system store) it
-    /// is expected that some of them might not be valid according to the rules rustls
-    /// implements. As long as a relatively limited number of certificates are affected,
-    /// this should not be a cause for concern. Use [`RootCertStore::add_parsable_certificates`]
-    /// in order to add as many valid roots as possible and to understand how many certificates
-    /// have been diagnosed as malformed.
-    pub fn add(&mut self, der: CertificateDer<'_>) -> Result<(), Error> {
-        self.roots.push(
-            extract_trust_anchor(&der)
-                .map_err(pki_error)?
-                .to_owned(),
-        );
-        Ok(())
-    }
-
     /// Parse the given DER-encoded certificates and add all that can be parsed
     /// in a best-effort fashion.
     ///
@@ -85,6 +57,34 @@ impl RootCertStore {
         );
 
         (valid_count, invalid_count)
+    }
+
+    /// Add a single DER-encoded certificate to the store.
+    ///
+    /// This is suitable for a small set of root certificates that are expected to parse
+    /// successfully. For large collections of roots (for example from a system store) it
+    /// is expected that some of them might not be valid according to the rules rustls
+    /// implements. As long as a relatively limited number of certificates are affected,
+    /// this should not be a cause for concern. Use [`RootCertStore::add_parsable_certificates`]
+    /// in order to add as many valid roots as possible and to understand how many certificates
+    /// have been diagnosed as malformed.
+    pub fn add(&mut self, der: CertificateDer<'_>) -> Result<(), Error> {
+        self.roots.push(
+            extract_trust_anchor(&der)
+                .map_err(pki_error)?
+                .to_owned(),
+        );
+        Ok(())
+    }
+
+    /// Return true if there are no certificates.
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+
+    /// Say how many certificates are in the container.
+    pub fn len(&self) -> usize {
+        self.roots.len()
     }
 }
 
