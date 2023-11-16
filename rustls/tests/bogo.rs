c8c46bea4d01d70a571a5644f7640cae7b4563e8
Joseph Birr-Pixton <jpixton@gmail.com>
2023-11-16 09:42:53 +0000

    Run bogo against both ring and aws-lc-rs providers

--- rustls/tests/bogo.rs
+++ rustls/tests/bogo.rs
@@ -3,16 +3,33 @@
 // and run.
 
 #[test]
-#[cfg(coverage)]
-fn run_bogo_tests() {
+#[ignore]
+fn run_bogo_tests_ring() {
     use std::process::Command;
 
     let rc = Command::new("./runme")
         .current_dir("../bogo")
+        .env("BOGO_SHIM_PROVIDER", "ring")
         .spawn()
         .expect("cannot run bogo/runme")
         .wait()
         .expect("cannot wait for bogo");
 
-    assert!(rc.success(), "bogo exited non-zero");
+    assert!(rc.success(), "bogo (ring) exited non-zero");
+}
+
+#[test]
+#[ignore]
+fn run_bogo_tests_aws_lc_rs() {
+    use std::process::Command;
+
+    let rc = Command::new("./runme")
+        .current_dir("../bogo")
+        .env("BOGO_SHIM_PROVIDER", "aws-lc-rs")
+        .spawn()
+        .expect("cannot run bogo/runme")
+        .wait()
+        .expect("cannot wait for bogo");
+
+    assert!(rc.success(), "bogo (aws-lc-rs) exited non-zero");
 }
