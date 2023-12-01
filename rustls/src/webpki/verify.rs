553f400785f1725c4d02511632a3053652cb9416
Daniel McCarney <daniel@binaryparadox.net>
2023-12-01 12:15:36 -0500

    docs: link to SignatureSchemes from WebPkiSupportedAlgorithms

--- rustls/src/webpki/verify.rs
+++ rustls/src/webpki/verify.rs
@@ -52,7 +52,7 @@ pub fn verify_server_name(
 }
 
 /// Describes which `webpki` signature verification algorithms are supported and
-/// how they map to TLS `SignatureScheme`s.
+/// how they map to TLS [`SignatureScheme`]s.
 #[derive(Clone, Copy)]
 #[allow(unreachable_pub)]
 pub struct WebPkiSupportedAlgorithms {
