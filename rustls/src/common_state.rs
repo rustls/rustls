d8a4625d852433d2ac50e4d01a66e89437f323ee
Geoffroy Couprie <apollo@geoffroycouprie.com>
2023-12-05 11:49:51 +0100

    remove the TLS 1.2 session ticket on DecryptError
    if for some reason the recorded session ticket is invalid or decoded
    incorrectly by the server, we can get into a case where the resumption
    handshake happens, and right after the ChangeCipherSpec message, the
    server sends an encrypted handhsake message using the invalid ticket,
    and the client rejects it with the BadRecordMAC alert.
    Unfortunately, if the calling code retries the connection, if it will
    try again with the same ticket and obtain the same result.
    This commit makes sure that if we fail to decrypt the first message, we
    will remove the session ticket for this server, to start from cratch on
    the next connection.

--- rustls/src/common_state.rs
+++ rustls/src/common_state.rs
@@ -630,6 +630,8 @@ pub(crate) trait State<Data>: Send + Sync {
     fn extract_secrets(&self) -> Result<PartiallyExtractedSecrets, Error> {
         Err(Error::HandshakeNotComplete)
     }
+
+    fn handle_decrypt_error(&self) {}
 }
 
 pub(crate) struct Context<'a, Data> {
