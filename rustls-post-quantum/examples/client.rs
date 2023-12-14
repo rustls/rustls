f553124b6aae0fe9c79bbf17a9383995206625c3
Joseph Birr-Pixton <jpixton@gmail.com>
2023-12-14 13:44:40 +0000

    Add example client

--- rustls-post-quantum/examples/client.rs
+++ rustls-post-quantum/examples/client.rs
@@ -1,9 +1,8 @@
-//! This is the simplest possible client using rustls that does something useful:
-//! it accepts the default configuration, loads some root certs, and then connects
-//! to rust-lang.org and issues a basic HTTP request.  The response is printed to stdout.
+//! This is the simplest possible client using rustls-postquantum, based on
+//! `simpleclient.rs`.
 //!
-//! It makes use of rustls::Stream to treat the underlying TLS connection as a basic
-//! bi-directional stream -- the underlying IO is performed transparently.
+//! It sends a HTTP request to pq.cloudflareresearch.com and prints the response to
+//! stdout.  Observe in that output: `kex=X25519Kyber768Draft00`
 //!
 //! Note that `unwrap()` is used to deal with networking errors; this is not something
 //! that is sensible outside of example code.
@@ -12,27 +11,30 @@ use std::io::{stdout, Read, Write};
 use std::net::TcpStream;
 use std::sync::Arc;
 
-use rustls::RootCertStore;
-
 fn main() {
-    let root_store = RootCertStore {
+    env_logger::init();
+    rustls_post_quantum::provider()
+        .install_default()
+        .unwrap();
+
+    let root_store = rustls::RootCertStore {
         roots: webpki_roots::TLS_SERVER_ROOTS.into(),
     };
-    let mut config = rustls::ClientConfig::builder()
+
+    let config = rustls::ClientConfig::builder()
         .with_root_certificates(root_store)
         .with_no_client_auth();
 
-    // Allow using SSLKEYLOGFILE.
-    config.key_log = Arc::new(rustls::KeyLogFile::new());
-
-    let server_name = "www.rust-lang.org".try_into().unwrap();
+    let server_name = "pq.cloudflareresearch.com"
+        .try_into()
+        .unwrap();
     let mut conn = rustls::ClientConnection::new(Arc::new(config), server_name).unwrap();
-    let mut sock = TcpStream::connect("www.rust-lang.org:443").unwrap();
+    let mut sock = TcpStream::connect("pq.cloudflareresearch.com:443").unwrap();
     let mut tls = rustls::Stream::new(&mut conn, &mut sock);
     tls.write_all(
         concat!(
-            "GET / HTTP/1.1\r\n",
-            "Host: www.rust-lang.org\r\n",
+            "GET /cdn-cgi/trace HTTP/1.0\r\n",
+            "Host: pq.cloudflareresearch.com\r\n",
             "Connection: close\r\n",
             "Accept-Encoding: identity\r\n",
             "\r\n"
