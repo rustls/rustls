7b9ba05a0c0a306a6c1bec65ee93ed1bb3e5dc46
Eloi DEMOLIS <eloi.demolis@clever-cloud.com>
2024-02-23 12:30:39 -0500

    macros: generalize enum_builder, generate get_array

--- rustls/src/msgs/macros.rs
+++ rustls/src/msgs/macros.rs
@@ -1,8 +1,14 @@
 /// A macro which defines an enum type.
 macro_rules! enum_builder {
+    ($(#[$comment:meta])* @U8 $($enum:tt)+) => {
+        enum_builder!(u8: get_u8 $(#[$comment])* $($enum)+);
+    };
+    ($(#[$comment:meta])* @U16 $($enum:tt)+) => {
+        enum_builder!(u16: get_u16 $(#[$comment])* $($enum)+);
+    };
     (
-    $(#[$comment:meta])*
-    @U8
+        $uint:ty: $get_uint:ident
+        $(#[$comment:meta])*
         $enum_vis:vis enum $enum_name:ident
         { $( $enum_var: ident => $enum_val: expr ),* $(,)? }
     ) => {
@@ -11,63 +17,24 @@ macro_rules! enum_builder {
         #[derive(Debug, PartialEq, Eq, Clone, Copy)]
         $enum_vis enum $enum_name {
             $( $enum_var),*
-            ,Unknown(u8)
+            ,Unknown($uint)
         }
         impl $enum_name {
-            $enum_vis fn get_u8(&self) -> u8 {
-                let x = self.clone();
-                match x {
+            $enum_vis fn $get_uint(&self) -> $uint {
+                match self {
                     $( $enum_name::$enum_var => $enum_val),*
-                    ,$enum_name::Unknown(x) => x
-                }
-            }
-        }
-        impl Codec<'_> for $enum_name {
-            // NOTE(allow) fully qualified Vec is only needed in no-std mode
-            #[allow(unused_qualifications)]
-            fn encode(&self, bytes: &mut alloc::vec::Vec<u8>) {
-                self.get_u8().encode(bytes);
-            }
-
-            fn read(r: &mut Reader) -> Result<Self, crate::error::InvalidMessage> {
-                match u8::read(r) {
-                    Ok(x) => Ok($enum_name::from(x)),
-                    Err(_) => Err(crate::error::InvalidMessage::MissingData(stringify!($enum_name))),
-                }
-            }
-        }
-        impl From<u8> for $enum_name {
-            fn from(x: u8) -> Self {
-                match x {
-                    $($enum_val => $enum_name::$enum_var),*
-                    , x => $enum_name::Unknown(x),
-                }
-            }
-        }
-    };
-    (
-    $(#[$comment:meta])*
-    @U16
-        $enum_vis:vis enum $enum_name:ident
-        { $( $enum_var: ident => $enum_val: expr ),* $(,)?}
-    ) => {
-        $(#[$comment])*
-        #[non_exhaustive]
-        #[derive(Debug, PartialEq, Eq, Clone, Copy)]
-        $enum_vis enum $enum_name {
-            $( $enum_var),*
-            ,Unknown(u16)
-        }
-        impl $enum_name {
-            $enum_vis fn get_u16(&self) -> u16 {
-                let x = self.clone();
-                match x {
-                    $( $enum_name::$enum_var => $enum_val),*
-                    ,$enum_name::Unknown(x) => x
+                    ,$enum_name::Unknown(x) => *x
                 }
             }
 
-            #[allow(dead_code)] // generated irrespective if there are callers
+            // NOTE(allow) generated irrespective if there are callers
+            #[allow(dead_code)]
+            $enum_vis fn get_array(&self) -> [u8; core::mem::size_of::<$uint>()] {
+                self.$get_uint().to_be_bytes()
+            }
+
+            // NOTE(allow) generated irrespective if there are callers
+            #[allow(dead_code)]
             $enum_vis fn as_str(&self) -> Option<&'static str> {
                 match self {
                     $( $enum_name::$enum_var => Some(stringify!($enum_var))),*
@@ -79,18 +46,18 @@ macro_rules! enum_builder {
             // NOTE(allow) fully qualified Vec is only needed in no-std mode
             #[allow(unused_qualifications)]
             fn encode(&self, bytes: &mut alloc::vec::Vec<u8>) {
-                self.get_u16().encode(bytes);
+                self.$get_uint().encode(bytes);
             }
 
             fn read(r: &mut Reader) -> Result<Self, crate::error::InvalidMessage> {
-                match u16::read(r) {
+                match <$uint>::read(r) {
                     Ok(x) => Ok($enum_name::from(x)),
                     Err(_) => Err(crate::error::InvalidMessage::MissingData(stringify!($enum_name))),
                 }
             }
         }
-        impl From<u16> for $enum_name {
-            fn from(x: u16) -> Self {
+        impl From<$uint> for $enum_name {
+            fn from(x: $uint) -> Self {
                 match x {
                     $($enum_val => $enum_name::$enum_var),*
                     , x => $enum_name::Unknown(x),
