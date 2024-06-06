dafc7ac2349c50095d57f60be36caf0fedb90c41
Joseph Birr-Pixton <jpixton@gmail.com>
2024-06-06 10:56:18 +0100

    Run bogo FIPS tests
    This is not straightforward because:
    
    - the tests want the FIPS selection to be at runtime, so we
      reconstruct the non-FIPS CryptoProvider and use that unless
      the `-fips-202205` is passed to the shim.
    - BoringSSL's FIPS evaluation is (presumably) older than aws-lc-rs,
      and doesn't include ED25519 (ref. FIPS 186-5) or P521.
    
    However, this does provider test coverage that:
    
    - X25519 is not usable
    - chacha20-poly1305 is not usable

--- rustls/tests/bogo.rs
+++ rustls/tests/bogo.rs
@@ -5,31 +5,31 @@
 #[test]
 #[ignore]
 fn run_bogo_tests_ring() {
-    use std::process::Command;
-
-    let rc = Command::new("./runme")
-        .current_dir("../bogo")
-        .env("BOGO_SHIM_PROVIDER", "ring")
-        .spawn()
-        .expect("cannot run bogo/runme")
-        .wait()
-        .expect("cannot wait for bogo");
-
-    assert!(rc.success(), "bogo (ring) exited non-zero");
+    run_bogo_tests("ring");
 }
 
 #[test]
 #[ignore]
 fn run_bogo_tests_aws_lc_rs() {
+    run_bogo_tests("aws-lc-rs");
+}
+
+#[test]
+#[ignore]
+fn run_bogo_tests_aws_lc_rs_fips() {
+    run_bogo_tests("aws-lc-rs-fips");
+}
+
+fn run_bogo_tests(provider: &str) {
     use std::process::Command;
 
     let rc = Command::new("./runme")
         .current_dir("../bogo")
-        .env("BOGO_SHIM_PROVIDER", "aws-lc-rs")
+        .env("BOGO_SHIM_PROVIDER", provider)
         .spawn()
         .expect("cannot run bogo/runme")
         .wait()
         .expect("cannot wait for bogo");
 
-    assert!(rc.success(), "bogo (aws-lc-rs) exited non-zero");
+    assert!(rc.success(), "bogo ({provider}) exited non-zero");
 }
