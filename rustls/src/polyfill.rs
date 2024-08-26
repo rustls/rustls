04574485ef173b3c9eae60987f6124ec406a5c58
Daniel McCarney <daniel@binaryparadox.net>
2024-08-26 14:58:04 -0400

    polyfill: allow dead code w/o built-in providers, no-std
    Only the `crypto/aws_lc_rs` and `crypto/ring` modules with std enabled
    use this polyfill. Since it's a small polyfill function we allow it to
    be dead code and avoid more complicated `cfg_attr` expressions.

--- rustls/src/polyfill.rs
+++ rustls/src/polyfill.rs
@@ -1,5 +1,6 @@
 /// Non-panicking `let (nonce, ciphertext) = ciphertext.split_at(...)`.
 // TODO(XXX): remove once stabilized in https://github.com/rust-lang/rust/issues/119128
+#[allow(dead_code)] // Complicated conditional compilation guards elided
 pub(crate) fn try_split_at(slice: &[u8], mid: usize) -> Option<(&[u8], &[u8])> {
     match mid > slice.len() {
         true => None,
