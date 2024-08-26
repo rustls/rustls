be3164cbad94f25e9f3fdc0079eb7bd156640de9
@brodycj - C. Jonathan Brody <cj@brodycj.com>
2024-08-26 00:13:54 -0400

    add XXX TODO comment to replace all use of pub_api_trait! with pub_api_trait_with_doc! & remove old pub_api_trait! macro

--- rustls/src/trait_macros.rs
+++ rustls/src/trait_macros.rs
@@ -7,6 +7,7 @@ macro_rules! pub_api_trait_with_doc {
     }
 }
 
+/////// XXX TODO REPLACE ALL USE OF THIS MACRO WITH pub_api_trait_with_doc! (with doc fixed) & REMOVE THIS MACRO
 /// pub trait - version with no doc - version that includes Send & Sync - supports use with alloc::sync::Arc
 #[cfg(not(feature = "withrcalias"))]
 macro_rules! pub_api_trait {
@@ -24,6 +25,7 @@ macro_rules! pub_api_trait_with_doc {
     }
 }
 
+/////// XXX TODO REPLACE ALL USE OF THIS MACRO WITH pub_api_trait_with_doc! (with doc fixed) & REMOVE THIS MACRO
 /// pub trait - version with no doc - version with no Send / Sync - supports use with alloc::rc::Rc
 #[cfg(feature = "withrcalias")]
 macro_rules! pub_api_trait {
