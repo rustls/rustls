64582a9835781e801a47464cdc4da5ca4f28f490
tahmid-23 <60953955+tahmid-23@users.noreply.github.com>
2024-09-03 12:28:47 -0400

    examples/simple_0rtt_client: allow connection with arbitrary endpoints/ca

--- examples/src/bin/simple_0rtt_client.rs
+++ examples/src/bin/simple_0rtt_client.rs
@@ -1,5 +1,7 @@
 //! This is an example client that uses rustls for TLS, and sends early 0-RTT data.
 //!
+//! Usage: cargo r --bin simple_0rtt_client --package rustls-examples [domain name] [port] [path/to/ca.cert]
+//!
 //! You may set the `SSLKEYLOGFILE` env var when using this example to write a
 //! log file with key material (insecure) for debugging purposes. See [`rustls::KeyLog`]
 //! for more information.
@@ -9,17 +11,19 @@
 
 use std::io::{BufRead, BufReader, Write};
 use std::net::TcpStream;
+use std::str::FromStr;
 use std::sync::Arc;
+use std::{env, fs};
 
 use rustls::pki_types::ServerName;
 use rustls::RootCertStore;
 
-fn start_connection(config: &Arc<rustls::ClientConfig>, domain_name: &str) {
+fn start_connection(config: &Arc<rustls::ClientConfig>, domain_name: &str, port: u16) {
     let server_name = ServerName::try_from(domain_name)
         .expect("invalid DNS name")
         .to_owned();
     let mut conn = rustls::ClientConnection::new(Arc::clone(config), server_name).unwrap();
-    let mut sock = TcpStream::connect(format!("{}:443", domain_name)).unwrap();
+    let mut sock = TcpStream::connect(format!("{}:{}", domain_name, port)).unwrap();
     sock.set_nodelay(true).unwrap();
     let request = format!(
         "GET / HTTP/1.1\r\n\
@@ -66,11 +70,30 @@ fn start_connection(config: &Arc<rustls::ClientConfig>, domain_name: &str) {
 fn main() {
     env_logger::init();
 
-    let root_store = RootCertStore::from_iter(
-        webpki_roots::TLS_SERVER_ROOTS
-            .iter()
-            .cloned(),
-    );
+    let mut args = env::args();
+    args.next();
+    let domain_name = args
+        .next()
+        .unwrap_or("jbp.io".to_owned());
+    let port = args
+        .next()
+        .map(|port| u16::from_str(&port).expect("invalid port"))
+        .unwrap_or(443);
+
+    let mut root_store = RootCertStore::empty();
+    if let Some(cafile) = args.next() {
+        let certfile = fs::File::open(cafile).expect("Cannot open CA file");
+        let mut reader = BufReader::new(certfile);
+        root_store.add_parsable_certificates(
+            rustls_pemfile::certs(&mut reader).map(|result| result.unwrap()),
+        );
+    } else {
+        root_store.extend(
+            webpki_roots::TLS_SERVER_ROOTS
+                .iter()
+                .cloned(),
+        )
+    }
 
     let mut config = rustls::ClientConfig::builder()
         .with_root_certificates(root_store)
@@ -87,7 +110,7 @@ fn main() {
     // second will use early data if the server supports it.
 
     println!("* Sending first request:");
-    start_connection(&config, "jbp.io");
+    start_connection(&config, &domain_name, port);
     println!("* Sending second request:");
-    start_connection(&config, "jbp.io");
+    start_connection(&config, &domain_name, port);
 }
