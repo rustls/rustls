516e8dea9114d66883c6d5d13fce1ef9d36801a5
Joe Birr-Pixton <jpixton@gmail.com>
2024-11-18 15:15:48 +0000

    `try_split_at`: note required MSRV

--- rustls/src/polyfill.rs
+++ rustls/src/polyfill.rs
@@ -1,5 +1,5 @@
 /// Non-panicking `let (nonce, ciphertext) = ciphertext.split_at(...)`.
-// TODO(XXX): remove once stabilized in https://github.com/rust-lang/rust/issues/119128
+// TODO(XXX): remove once MSRV reaches 1.80
 #[allow(dead_code)] // Complicated conditional compilation guards elided
 pub(crate) fn try_split_at(slice: &[u8], mid: usize) -> Option<(&[u8], &[u8])> {
     match mid > slice.len() {
