8022b99b6f948011f11f6e3bd0e890258d7e60c4
Joe Birr-Pixton <jpixton@gmail.com>
2025-02-13 10:55:35 +0000

    Correct equality of `UnknownRevocationStatus`

--- rustls/src/error.rs
+++ rustls/src/error.rs
@@ -487,6 +487,7 @@ impl PartialEq<Self> for CertificateError {
             ) => le.eq(re) && lp.eq(rp),
             (InvalidPurpose, InvalidPurpose) => true,
             (ApplicationVerificationFailure, ApplicationVerificationFailure) => true,
+            (UnknownRevocationStatus, UnknownRevocationStatus) => true,
             (ExpiredRevocationList, ExpiredRevocationList) => true,
             (
                 ExpiredRevocationListDetail {
@@ -831,6 +832,8 @@ mod tests {
         assert_eq!(Revoked, Revoked);
         assert_eq!(UnhandledCriticalExtension, UnhandledCriticalExtension);
         assert_eq!(UnknownIssuer, UnknownIssuer);
+        assert_eq!(ExpiredRevocationList, ExpiredRevocationList);
+        assert_eq!(UnknownRevocationStatus, UnknownRevocationStatus);
         let detail = ExpiredRevocationListDetail {
             time: UnixTime::since_unix_epoch(Duration::from_secs(1234)),
             next_update: UnixTime::since_unix_epoch(Duration::from_secs(123)),
