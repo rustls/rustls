328b854852fc3cde3f75cd3fafa60a2442db79c9
Sean Lynch <42618346+swlynch99@users.noreply.github.com>
2025-03-08 14:11:45 -0800

    Add test cases for ExternalConnection key updates

---
+++ rustls/tests/runners/external.rs
@@ -0,0 +1,20 @@
+#[macro_use]
+mod macros;
+
+#[cfg(feature = "ring")]
+#[path = "."]
+mod tests_with_ring {
+    provider_ring!();
+
+    #[path = "../external.rs"]
+    mod tests;
+}
+
+#[cfg(feature = "aws_lc_rs")]
+#[path = "."]
+mod tests_with_aws_lc_rs {
+    provider_aws_lc_rs!();
+
+    #[path = "../external.rs"]
+    mod tests;
+}
