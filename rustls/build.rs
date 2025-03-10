9546f02a21577dbd25ac121e5da47ac0c253a7ec
Joe Birr-Pixton <jpixton@gmail.com>
2025-03-10 15:59:16 +0000

    Prefer to put check-cfg data in Cargo.toml

--- rustls/build.rs
+++ rustls/build.rs
@@ -4,15 +4,10 @@
 /// See the comment in lib.rs to understand why we need this.
 
 #[cfg_attr(feature = "read_buf", rustversion::not(nightly))]
-fn main() {
-    println!("cargo:rustc-check-cfg=cfg(bench)");
-    println!("cargo:rustc-check-cfg=cfg(read_buf)");
-}
+fn main() {}
 
 #[cfg(feature = "read_buf")]
 #[rustversion::nightly]
 fn main() {
-    println!("cargo:rustc-check-cfg=cfg(bench)");
-    println!("cargo:rustc-check-cfg=cfg(read_buf)");
     println!("cargo:rustc-cfg=read_buf");
 }
