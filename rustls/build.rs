7d0a83c042b84b617c59bafbbad7c4dc5e1ee285
Joe Birr-Pixton <jpixton@gmail.com>
2025-05-15 20:31:21 +0200

    build.rs: use correct comment style

--- rustls/build.rs
+++ rustls/build.rs
@@ -1,7 +1,7 @@
-/// This build script allows us to enable the `read_buf` language feature only
-/// for Rust Nightly.
-///
-/// See the comment in lib.rs to understand why we need this.
+//! This build script allows us to enable the `read_buf` language feature only
+//! for Rust Nightly.
+//!
+//! See the comment in lib.rs to understand why we need this.
 
 #[cfg_attr(feature = "read_buf", rustversion::not(nightly))]
 fn main() {}
