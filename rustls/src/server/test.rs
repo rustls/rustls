739617399e68d6645b5a96aff4eff1a5ae6b6964
Joe Birr-Pixton <jpixton@gmail.com>
2025-05-19 09:03:05 +0100

    server::test: port `server_ignores_sni_with_ip_address`

--- rustls/src/server/test.rs
+++ rustls/src/server/test.rs
@@ -3,7 +3,8 @@ use std::vec;
 
 use super::ServerConnectionData;
 use crate::common_state::Context;
-use crate::msgs::enums::Compression;
+use crate::msgs::codec::{Codec, LengthPrefixedBuffer, ListLength};
+use crate::msgs::enums::{Compression, ExtensionType};
 use crate::msgs::handshake::{
     ClientExtension, ClientHelloPayload, HandshakeMessagePayload, HandshakePayload, Random,
     SessionId,
@@ -20,6 +21,15 @@ fn null_compression_required() {
         }),
         Err(PeerIncompatible::NullCompressionRequired.into()),
     );
+}
+
+#[test]
+fn server_ignores_sni_with_ip_address() {
+    let mut ch = minimal_client_hello();
+    ch.extensions
+        .push(ClientExtension::read_bytes(&sni_extension(&[b"1.1.1.1"])).unwrap());
+    std::println!("{:?}", ch.extensions);
+    assert_eq!(test_process_client_hello(ch), Ok(()));
 }
 
 fn test_process_client_hello(hello: ClientHelloPayload) -> Result<(), Error> {
@@ -51,4 +61,20 @@ fn minimal_client_hello() -> ClientHelloPayload {
         compression_methods: vec![Compression::Null],
         extensions: vec![ClientExtension::SignatureAlgorithms(vec![])],
     }
+}
+
+fn sni_extension(names: &[&[u8]]) -> Vec<u8> {
+    let mut r = Vec::new();
+    ExtensionType::ServerName.encode(&mut r);
+    let outer = LengthPrefixedBuffer::new(ListLength::U16, &mut r);
+    let name_items = LengthPrefixedBuffer::new(ListLength::U16, outer.buf);
+    for name in names {
+        name_items.buf.push(0);
+        let host_name = LengthPrefixedBuffer::new(ListLength::U16, name_items.buf);
+        host_name.buf.extend_from_slice(name);
+        drop(host_name);
+    }
+    drop(name_items);
+    drop(outer);
+    r
 }
