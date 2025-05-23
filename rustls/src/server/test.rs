78e12d3367c74330ebee2994246b79ee4eabdb33
Joe Birr-Pixton <jpixton@gmail.com>
2025-05-23 16:49:08 +0100

    Convert `test_server_rejects_no_extended_master_secret(...)` to unit test

--- rustls/src/server/test.rs
+++ rustls/src/server/test.rs
@@ -3,8 +3,9 @@ use std::vec;
 
 use super::ServerConnectionData;
 use crate::common_state::Context;
+use crate::enums::{CipherSuite, SignatureScheme};
 use crate::msgs::codec::{Codec, LengthPrefixedBuffer, ListLength};
-use crate::msgs::enums::{Compression, ExtensionType};
+use crate::msgs::enums::{Compression, ExtensionType, NamedGroup};
 use crate::msgs::handshake::{
     ClientExtension, ClientHelloPayload, HandshakeMessagePayload, HandshakePayload, Random,
     SessionId,
@@ -66,14 +67,78 @@ fn test_process_client_hello(hello: ClientHelloPayload) -> Result<(), Error> {
     .map(|_| ())
 }
 
+#[macro_rules_attribute::apply(test_for_each_provider)]
+mod tests {
+    use super::super::*;
+    use crate::pki_types::pem::PemObject;
+    use crate::pki_types::{CertificateDer, PrivateKeyDer};
+    use crate::server::{ServerConfig, ServerConnection};
+    use crate::sync::Arc;
+    use crate::version;
+
+    #[cfg(feature = "tls12")]
+    #[test]
+    fn test_server_rejects_no_extended_master_secret_extension_when_require_ems_or_fips() {
+        let provider = super::provider::default_provider();
+        let mut config = ServerConfig::builder_with_provider(provider.into())
+            .with_protocol_versions(&[&version::TLS12])
+            .unwrap()
+            .with_no_client_auth()
+            .with_single_cert(server_cert(), server_key())
+            .unwrap();
+
+        if config.provider.fips() {
+            assert!(config.require_ems);
+        } else {
+            config.require_ems = true;
+        }
+        let config = config.into();
+        let mut conn = ServerConnection::new(Arc::clone(&config)).unwrap();
+
+        let sh = Message {
+            version: ProtocolVersion::TLSv1_3,
+            payload: MessagePayload::handshake(HandshakeMessagePayload {
+                typ: HandshakeType::ClientHello,
+                payload: HandshakePayload::ClientHello(minimal_client_hello()),
+            }),
+        };
+        conn.read_tls(&mut sh.into_wire_bytes().as_slice())
+            .unwrap();
+
+        assert_eq!(
+            conn.process_new_packets(),
+            Err(Error::PeerIncompatible(
+                PeerIncompatible::ExtendedMasterSecretExtensionRequired
+            ))
+        );
+    }
+
+    fn server_key() -> PrivateKeyDer<'static> {
+        PrivateKeyDer::from_pem_reader(
+            &mut include_bytes!("../../../test-ca/rsa-2048/end.key").as_slice(),
+        )
+        .unwrap()
+    }
+
+    fn server_cert() -> Vec<CertificateDer<'static>> {
+        vec![
+            CertificateDer::from(&include_bytes!("../../../test-ca/rsa-2048/end.der")[..]),
+            CertificateDer::from(&include_bytes!("../../../test-ca/rsa-2048/inter.der")[..]),
+        ]
+    }
+}
+
 fn minimal_client_hello() -> ClientHelloPayload {
     ClientHelloPayload {
         client_version: ProtocolVersion::TLSv1_3,
         random: Random::from([0u8; 32]),
         session_id: SessionId::empty(),
-        cipher_suites: vec![],
+        cipher_suites: vec![CipherSuite::TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256],
         compression_methods: vec![Compression::Null],
-        extensions: vec![ClientExtension::SignatureAlgorithms(vec![])],
+        extensions: vec![
+            ClientExtension::SignatureAlgorithms(vec![SignatureScheme::RSA_PSS_SHA256]),
+            ClientExtension::NamedGroups(vec![NamedGroup::X25519, NamedGroup::secp256r1]),
+        ],
     }
 }
 
