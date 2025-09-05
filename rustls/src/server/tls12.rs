f06de77eef888eb50fbb4abb589aae35a5b15206
Joe Birr-Pixton <jpixton@gmail.com>
2025-09-05 08:34:53 +0100

    Exclude functions that are unreachable from coverage

--- rustls/src/server/tls12.rs
+++ rustls/src/server/tls12.rs
@@ -1026,12 +1026,14 @@ impl State<ServerConnectionData> for ExpectTraffic {
 }
 
 impl KernelState for ExpectTraffic {
+    #[cfg_attr(coverage_nightly, coverage(off))]
     fn update_secrets(&mut self, _: Direction) -> Result<ConnectionTrafficSecrets, Error> {
         Err(Error::Unreachable(
             "TLS 1.2 connections do not support traffic secret updates",
         ))
     }
 
+    #[cfg_attr(coverage_nightly, coverage(off))]
     fn handle_new_session_ticket(
         &mut self,
         _cx: &mut KernelContext<'_>,
