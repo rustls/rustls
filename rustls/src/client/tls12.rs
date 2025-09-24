d7889b03c27530b994a651eb51265ca549f4b0a6
Dirkjan Ochtman <dirkjan@ochtman.nl>
2025-09-24 17:45:24 +0200

    client: only retain certificate chain from CertifiedKey

--- rustls/src/client/tls12.rs
+++ rustls/src/client/tls12.rs
@@ -942,13 +942,9 @@ impl State<ClientConnectionData> for ExpectServerDone<'_> {
         if let Some(client_auth) = &st.client_auth {
             let certs = match client_auth {
                 ClientAuthDetails::Empty { .. } => CertificateChain::default(),
-                ClientAuthDetails::Verify { certkey, .. } => CertificateChain(
-                    certkey
-                        .cert_chain
-                        .iter()
-                        .cloned()
-                        .collect(),
-                ),
+                ClientAuthDetails::Verify { cert_chain, .. } => {
+                    CertificateChain(cert_chain.iter().cloned().collect())
+                }
             };
             emit_certificate(&mut st.transcript, certs, cx.common);
         }
