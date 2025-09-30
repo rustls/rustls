425b0dc05861075afcf7885771603862b974c537
Joe Birr-Pixton <jpixton@gmail.com>
2025-09-30 16:03:52 +0100

    bogo: tighten uncompressed points error

--- bogo/src/main.rs
+++ bogo/src/main.rs
@@ -1225,6 +1225,9 @@ fn handle_err(opts: &Options, err: Error) -> ! {
         Error::PeerMisbehaved(PeerMisbehaved::IncorrectBinder) => {
             quit(":DECRYPTION_FAILED_OR_BAD_RECORD_MAC:")
         }
+        Error::PeerMisbehaved(PeerMisbehaved::ServerHelloMustOfferUncompressedEcPoints) => {
+            quit(":SERVER_HELLO_MUST_OFFER_UNCOMPRESSED_EC_POINTS:")
+        }
         Error::PeerMisbehaved(_) => quit(":PEER_MISBEHAVIOUR:"),
         Error::AlertReceived(AlertDescription::UnexpectedMessage) => quit(":BAD_ALERT:"),
         Error::AlertReceived(AlertDescription::DecompressionFailure) => {
