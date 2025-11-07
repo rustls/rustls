1a426f8134271e45f7b924ea357bb78d370820a8
Dirkjan Ochtman <dirkjan@ochtman.nl>
2025-11-07 09:12:05 +0100

    tests: port server tests to TEST_PROVIDERS

--- rustls/src/test_macros.rs
+++ rustls/src/test_macros.rs
@@ -1,31 +1,5 @@
 //! Macros used for unit testing.
 
-/// Instantiate the given test functions once for each built-in provider.
-///
-/// The selected provider module is bound as `provider`; you can rely on this
-/// having the union of the items common to the `crypto::ring` and
-/// `crypto::aws_lc_rs` modules.
-#[cfg(test)]
-macro_rules! test_for_each_provider {
-    ($($tt:tt)+) => {
-        #[cfg(feature = "ring")]
-        mod test_with_ring {
-            use crate::crypto::ring as provider;
-            #[allow(unused_imports)]
-            use super::*;
-            $($tt)+
-        }
-
-        #[cfg(feature = "aws-lc-rs")]
-        mod test_with_aws_lc_rs {
-            use crate::crypto::aws_lc_rs as provider;
-            #[allow(unused_imports)]
-            use super::*;
-            $($tt)+
-        }
-    };
-}
-
 /// Instantiate the given benchmark functions once for each built-in provider.
 ///
 /// The selected provider module is bound as `provider`; you can rely on this
@@ -50,15 +24,6 @@ macro_rules! bench_for_each_provider {
             $($tt)+
         }
     };
-}
-
-#[cfg(test)]
-#[macro_rules_attribute::apply(test_for_each_provider)]
-mod tests {
-    #[test]
-    fn test_each_provider() {
-        std::println!("provider is {:?}", super::provider::DEFAULT_PROVIDER);
-    }
 }
 
 #[cfg(all(test, bench))]
