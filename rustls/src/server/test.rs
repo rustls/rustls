0bd1dbc44584db4a6c7f494383b8ee53e6d6276c
Dirkjan Ochtman <dirkjan@ochtman.nl>
2025-11-07 09:12:05 +0100

    tests: port server tests to TEST_PROVIDERS

--- rustls/src/server/test.rs
+++ rustls/src/server/test.rs
@@ -1,11 +1,22 @@
-use std::prelude::v1::*;
+#![cfg(any(feature = "aws-lc-rs", feature = "ring"))]
+
+use alloc::borrow::Cow;
+use alloc::boxed::Box;
 use std::vec;
 
 use super::ServerConnectionData;
 use super::hs::ClientHelloInput;
-use crate::common_state::Context;
-use crate::enums::{CipherSuite, ProtocolVersion, SignatureScheme};
+use crate::common_state::{CommonState, Context, KxState, Side};
+use crate::crypto::cipher::FakeAead;
+use crate::crypto::hash::FakeHash;
+use crate::crypto::tls12::FakePrf;
+use crate::crypto::{
+    ActiveKeyExchange, Credentials, CryptoProvider, Identity, KeyExchangeAlgorithm, SharedSecret,
+    SingleCredential, StartedKeyExchange, SupportedKxGroup, tls12_only,
+};
+use crate::enums::{CertificateType, CipherSuite, ProtocolVersion, SignatureScheme};
 use crate::error::{Error, PeerIncompatible};
+use crate::ffdhe_groups::FfdheGroup;
 use crate::msgs::base::PayloadU16;
 use crate::msgs::enums::{Compression, NamedGroup};
 use crate::msgs::handshake::{
@@ -13,7 +24,14 @@ use crate::msgs::handshake::{
     Random, SessionId, SupportedProtocolVersions,
 };
 use crate::msgs::message::{Message, MessagePayload};
-use crate::{CommonState, Side};
+use crate::pki_types::pem::PemObject;
+use crate::pki_types::{CertificateDer, PrivateKeyDer};
+use crate::server::{ServerConfig, ServerConnection};
+use crate::suites::CipherSuiteCommon;
+use crate::sync::Arc;
+use crate::tls12::Tls12CipherSuite;
+use crate::version::TLS12_VERSION;
+use crate::{TEST_PROVIDERS, ffdhe_groups};
 
 #[test]
 fn null_compression_required() {
@@ -46,31 +64,10 @@ fn test_process_client_hello(hello: ClientHelloPayload) -> Result<(), Error> {
     .map(|_| ())
 }
 
-#[macro_rules_attribute::apply(test_for_each_provider)]
-mod tests {
-    use alloc::borrow::Cow;
-
-    use super::super::*;
-    use crate::common_state::KxState;
-    use crate::crypto::cipher::FakeAead;
-    use crate::crypto::hash::FakeHash;
-    use crate::crypto::tls12::FakePrf;
-    use crate::crypto::{
-        ActiveKeyExchange, Credentials, CryptoProvider, Identity, KeyExchangeAlgorithm,
-        SingleCredential, StartedKeyExchange, SupportedKxGroup,
-    };
-    use crate::enums::CertificateType;
-    use crate::ffdhe_groups::FfdheGroup;
-    use crate::pki_types::pem::PemObject;
-    use crate::pki_types::{CertificateDer, PrivateKeyDer};
-    use crate::server::{ServerConfig, ServerConnection};
-    use crate::sync::Arc;
-    use crate::version::TLS12_VERSION;
-    use crate::{CipherSuiteCommon, Tls12CipherSuite};
-
-    #[test]
-    fn test_server_rejects_no_extended_master_secret_extension_when_require_ems_or_fips() {
-        let provider = super::provider::DEFAULT_TLS12_PROVIDER;
+#[test]
+fn test_server_rejects_no_extended_master_secret_extension_when_require_ems_or_fips() {
+    for &provider in TEST_PROVIDERS {
+        let provider = tls12_only(provider.clone());
         let mut config = ServerConfig::builder(provider.into())
             .with_no_client_auth()
             .with_single_cert(server_identity(), server_key())
@@ -103,16 +100,15 @@ mod tests {
             ))
         );
     }
+}
 
-    #[test]
-    fn server_picks_ffdhe_group_when_clienthello_has_no_ffdhe_group_in_groups_ext() {
-        let config = ServerConfig::builder(
-            CryptoProvider {
-                tls13_cipher_suites: Default::default(),
-                ..ffdhe_provider()
-            }
-            .into(),
-        )
+#[test]
+fn server_picks_ffdhe_group_when_clienthello_has_no_ffdhe_group_in_groups_ext() {
+    for &provider in TEST_PROVIDERS {
+        let config = ServerConfig::builder(Arc::new(CryptoProvider {
+            tls13_cipher_suites: Cow::default(),
+            ..ffdhe_provider(provider.clone())
+        }))
         .with_no_client_auth()
         .with_single_cert(server_identity(), server_key())
         .unwrap();
@@ -129,16 +125,15 @@ mod tests {
             ch,
         );
     }
+}
 
-    #[test]
-    fn server_picks_ffdhe_group_when_clienthello_has_no_groups_ext() {
-        let config = ServerConfig::builder(
-            CryptoProvider {
-                tls13_cipher_suites: Default::default(),
-                ..ffdhe_provider()
-            }
-            .into(),
-        )
+#[test]
+fn server_picks_ffdhe_group_when_clienthello_has_no_groups_ext() {
+    for &provider in TEST_PROVIDERS {
+        let config = ServerConfig::builder(Arc::new(CryptoProvider {
+            tls13_cipher_suites: Cow::default(),
+            ..ffdhe_provider(provider.clone())
+        }))
         .with_no_client_auth()
         .with_single_cert(server_identity(), server_key())
         .unwrap();
@@ -156,16 +151,15 @@ mod tests {
             ch,
         );
     }
+}
 
-    #[test]
-    fn server_accepts_client_with_no_ecpoints_extension_and_only_ffdhe_cipher_suites() {
-        let config = ServerConfig::builder(
-            CryptoProvider {
-                tls13_cipher_suites: Default::default(),
-                ..ffdhe_provider()
-            }
-            .into(),
-        )
+#[test]
+fn server_accepts_client_with_no_ecpoints_extension_and_only_ffdhe_cipher_suites() {
+    for &provider in TEST_PROVIDERS {
+        let config = ServerConfig::builder(Arc::new(CryptoProvider {
+            tls13_cipher_suites: Cow::default(),
+            ..ffdhe_provider(provider.clone())
+        }))
         .with_no_client_auth()
         .with_single_cert(server_identity(), server_key())
         .unwrap();
@@ -183,29 +177,35 @@ mod tests {
             ch,
         );
     }
+}
 
-    fn server_chooses_ffdhe_group_for_client_hello(
-        mut conn: ServerConnection,
-        client_hello: ClientHelloPayload,
-    ) {
-        let ch = Message {
-            version: ProtocolVersion::TLSv1_3,
-            payload: MessagePayload::handshake(HandshakeMessagePayload(
-                HandshakePayload::ClientHello(client_hello),
-            )),
+fn server_chooses_ffdhe_group_for_client_hello(
+    mut conn: ServerConnection,
+    client_hello: ClientHelloPayload,
+) {
+    let ch = Message {
+        version: ProtocolVersion::TLSv1_3,
+        payload: MessagePayload::handshake(HandshakeMessagePayload(HandshakePayload::ClientHello(
+            client_hello,
+        ))),
+    };
+    conn.read_tls(&mut ch.into_wire_bytes().as_slice())
+        .unwrap();
+    conn.process_new_packets().unwrap();
+
+    let KxState::Start(skxg) = &conn.kx_state else {
+        panic!("unexpected kx_state");
+    };
+    assert_eq!(skxg.name(), FAKE_FFDHE_GROUP.name());
+}
+
+#[test]
+fn test_server_requiring_rpk_client_rejects_x509_client() {
+    for &provider in TEST_PROVIDERS {
+        let Some(server_config) = server_config_for_rpk(provider.clone()) else {
+            continue;
         };
-        conn.read_tls(&mut ch.into_wire_bytes().as_slice())
-            .unwrap();
-        conn.process_new_packets().unwrap();
 
-        let KxState::Start(skxg) = &conn.kx_state else {
-            panic!("unexpected kx_state");
-        };
-        assert_eq!(skxg.name(), FAKE_FFDHE_GROUP.name());
-    }
-
-    #[test]
-    fn test_server_requiring_rpk_client_rejects_x509_client() {
         let mut ch = minimal_client_hello();
         ch.extensions.client_certificate_types = Some(vec![CertificateType::X509]);
         let ch = Message {
@@ -215,7 +215,7 @@ mod tests {
             )),
         };
 
-        let mut conn = ServerConnection::new(server_config_for_rpk().into()).unwrap();
+        let mut conn = ServerConnection::new(Arc::new(server_config)).unwrap();
         conn.read_tls(&mut ch.into_wire_bytes().as_slice())
             .unwrap();
         assert_eq!(
@@ -223,9 +223,15 @@ mod tests {
             PeerIncompatible::IncorrectCertificateTypeExtension.into(),
         );
     }
+}
 
-    #[test]
-    fn test_rpk_only_server_rejects_x509_only_client() {
+#[test]
+fn test_rpk_only_server_rejects_x509_only_client() {
+    for &provider in TEST_PROVIDERS {
+        let Some(server_config) = server_config_for_rpk(provider.clone()) else {
+            continue;
+        };
+
         let mut ch = minimal_client_hello();
         ch.extensions.server_certificate_types = Some(vec![CertificateType::X509]);
         let ch = Message {
@@ -235,121 +241,123 @@ mod tests {
             )),
         };
 
-        let mut conn = ServerConnection::new(server_config_for_rpk().into()).unwrap();
+        let mut conn = ServerConnection::new(Arc::new(server_config)).unwrap();
         conn.read_tls(&mut ch.into_wire_bytes().as_slice())
             .unwrap();
+
         assert_eq!(
             conn.process_new_packets().unwrap_err(),
             PeerIncompatible::IncorrectCertificateTypeExtension.into(),
         );
     }
-
-    fn server_config_for_rpk() -> ServerConfig {
-        ServerConfig::builder(
-            CryptoProvider {
-                kx_groups: Cow::Owned(vec![super::provider::kx_group::X25519]),
-                ..super::provider::DEFAULT_TLS12_PROVIDER
-            }
-            .into(),
-        )
-        .with_no_client_auth()
-        .with_server_credential_resolver(Arc::new(SingleCredential::from(server_credentials())))
-        .unwrap()
-    }
-
-    fn server_credentials() -> Credentials {
-        let key = super::provider::DEFAULT_PROVIDER
-            .key_provider
-            .load_private_key(server_key())
-            .unwrap();
-        let identity = Arc::from(Identity::RawPublicKey(
-            key.public_key().unwrap().into_owned(),
-        ));
-        Credentials::new_unchecked(identity, key)
-    }
-
-    fn server_key() -> PrivateKeyDer<'static> {
-        PrivateKeyDer::from_pem_reader(
-            &mut include_bytes!("../../../test-ca/rsa-2048/end.key").as_slice(),
-        )
-        .unwrap()
-    }
-
-    fn server_identity() -> Arc<Identity<'static>> {
-        Arc::new(
-            Identity::from_cert_chain(vec![
-                CertificateDer::from(&include_bytes!("../../../test-ca/rsa-2048/end.der")[..]),
-                CertificateDer::from(&include_bytes!("../../../test-ca/rsa-2048/inter.der")[..]),
-            ])
-            .unwrap(),
-        )
-    }
-
-    fn ffdhe_provider() -> CryptoProvider {
-        CryptoProvider {
-            kx_groups: Cow::Owned(vec![FAKE_FFDHE_GROUP]),
-            tls12_cipher_suites: Cow::Owned(vec![&TLS_DHE_RSA_WITH_AES_128_GCM_SHA256]),
-            ..super::provider::DEFAULT_PROVIDER
-        }
-    }
-
-    static FAKE_FFDHE_GROUP: &'static dyn SupportedKxGroup = &FakeFfdheGroup;
-
-    #[derive(Debug)]
-    struct FakeFfdheGroup;
-
-    impl SupportedKxGroup for FakeFfdheGroup {
-        fn ffdhe_group(&self) -> Option<FfdheGroup<'static>> {
-            Some(crate::ffdhe_groups::FFDHE2048)
-        }
-
-        fn name(&self) -> NamedGroup {
-            NamedGroup::FFDHE2048
-        }
-
-        fn start(&self) -> Result<StartedKeyExchange, Error> {
-            Ok(StartedKeyExchange::Single(Box::new(ActiveFakeFfdhe)))
-        }
-    }
-
-    #[derive(Debug)]
-    struct ActiveFakeFfdhe;
-
-    impl ActiveKeyExchange for ActiveFakeFfdhe {
-        #[cfg_attr(coverage_nightly, coverage(off))]
-        fn complete(
-            self: Box<Self>,
-            _peer_pub_key: &[u8],
-        ) -> Result<crate::crypto::SharedSecret, Error> {
-            todo!()
-        }
-
-        fn pub_key(&self) -> &[u8] {
-            b"ActiveFakeFfdhe pub key"
-        }
-
-        fn ffdhe_group(&self) -> Option<FfdheGroup<'static>> {
-            Some(crate::ffdhe_groups::FFDHE2048)
-        }
-
-        fn group(&self) -> NamedGroup {
-            NamedGroup::FFDHE2048
-        }
-    }
-
-    static TLS_DHE_RSA_WITH_AES_128_GCM_SHA256: Tls12CipherSuite = Tls12CipherSuite {
-        common: CipherSuiteCommon {
-            suite: CipherSuite::TLS_DHE_RSA_WITH_AES_128_GCM_SHA256,
-            hash_provider: &FakeHash,
-            confidentiality_limit: 1,
-        },
-        kx: KeyExchangeAlgorithm::DHE,
-        protocol_version: TLS12_VERSION,
-        prf_provider: &FakePrf,
-        sign: &[SignatureScheme::RSA_PKCS1_SHA256],
-        aead_alg: &FakeAead,
-    };
 }
+
+fn server_config_for_rpk(provider: CryptoProvider) -> Option<ServerConfig> {
+    let provider = CryptoProvider {
+        kx_groups: Cow::Owned(vec![
+            provider.find_kx_group(NamedGroup::X25519, ProtocolVersion::TLSv1_2)?,
+        ]),
+        ..provider
+    };
+
+    let credentials = SingleCredential::from(server_credentials(&provider));
+    Some(
+        ServerConfig::builder(Arc::new(provider))
+            .with_no_client_auth()
+            .with_server_credential_resolver(Arc::new(credentials))
+            .unwrap(),
+    )
+}
+
+fn server_credentials(provider: &CryptoProvider) -> Credentials {
+    let key = provider
+        .key_provider
+        .load_private_key(server_key())
+        .unwrap();
+    let identity = Arc::from(Identity::RawPublicKey(
+        key.public_key().unwrap().into_owned(),
+    ));
+    Credentials::new_unchecked(identity, key)
+}
+
+fn server_key() -> PrivateKeyDer<'static> {
+    PrivateKeyDer::from_pem_reader(
+        &mut include_bytes!("../../../test-ca/rsa-2048/end.key").as_slice(),
+    )
+    .unwrap()
+}
+
+fn server_identity() -> Arc<Identity<'static>> {
+    Arc::new(
+        Identity::from_cert_chain(vec![
+            CertificateDer::from(&include_bytes!("../../../test-ca/rsa-2048/end.der")[..]),
+            CertificateDer::from(&include_bytes!("../../../test-ca/rsa-2048/inter.der")[..]),
+        ])
+        .unwrap(),
+    )
+}
+
+fn ffdhe_provider(provider: CryptoProvider) -> CryptoProvider {
+    CryptoProvider {
+        kx_groups: Cow::Owned(vec![FAKE_FFDHE_GROUP]),
+        tls12_cipher_suites: Cow::Owned(vec![&TLS_DHE_RSA_WITH_AES_128_GCM_SHA256]),
+        ..provider
+    }
+}
+
+static FAKE_FFDHE_GROUP: &'static dyn SupportedKxGroup = &FakeFfdheGroup;
+
+#[derive(Debug)]
+struct FakeFfdheGroup;
+
+impl SupportedKxGroup for FakeFfdheGroup {
+    fn ffdhe_group(&self) -> Option<FfdheGroup<'static>> {
+        Some(ffdhe_groups::FFDHE2048)
+    }
+
+    fn name(&self) -> NamedGroup {
+        NamedGroup::FFDHE2048
+    }
+
+    fn start(&self) -> Result<StartedKeyExchange, Error> {
+        Ok(StartedKeyExchange::Single(Box::new(ActiveFakeFfdhe)))
+    }
+}
+
+#[derive(Debug)]
+struct ActiveFakeFfdhe;
+
+impl ActiveKeyExchange for ActiveFakeFfdhe {
+    #[cfg_attr(coverage_nightly, coverage(off))]
+    fn complete(self: Box<Self>, _peer_pub_key: &[u8]) -> Result<SharedSecret, Error> {
+        todo!()
+    }
+
+    fn pub_key(&self) -> &[u8] {
+        b"ActiveFakeFfdhe pub key"
+    }
+
+    fn ffdhe_group(&self) -> Option<FfdheGroup<'static>> {
+        Some(ffdhe_groups::FFDHE2048)
+    }
+
+    fn group(&self) -> NamedGroup {
+        NamedGroup::FFDHE2048
+    }
+}
+
+static TLS_DHE_RSA_WITH_AES_128_GCM_SHA256: Tls12CipherSuite = Tls12CipherSuite {
+    common: CipherSuiteCommon {
+        suite: CipherSuite::TLS_DHE_RSA_WITH_AES_128_GCM_SHA256,
+        hash_provider: &FakeHash,
+        confidentiality_limit: 1,
+    },
+    kx: KeyExchangeAlgorithm::DHE,
+    protocol_version: TLS12_VERSION,
+    prf_provider: &FakePrf,
+    sign: &[SignatureScheme::RSA_PKCS1_SHA256],
+    aead_alg: &FakeAead,
+};
 
 fn minimal_client_hello() -> ClientHelloPayload {
     ClientHelloPayload {
