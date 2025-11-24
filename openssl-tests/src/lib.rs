df15cc0b0f2a59c391b39e4246267b6dcd819a8e
Joe Birr-Pixton <jpixton@gmail.com>
2025-11-24 13:11:40 +0000

    openssl-tests: align with workspace lints

--- openssl-tests/src/lib.rs
+++ openssl-tests/src/lib.rs
@@ -1,17 +1,4 @@
 #![cfg(test)]
-#![warn(
-    clippy::alloc_instead_of_core,
-    clippy::manual_let_else,
-    clippy::std_instead_of_core,
-    clippy::use_self,
-    clippy::upper_case_acronyms,
-    elided_lifetimes_in_paths,
-    trivial_numeric_casts,
-    unreachable_pub,
-    unused_import_braces,
-    unused_extern_crates,
-    unused_qualifications
-)]
 
 mod early_exporter;
 mod ffdhe;
