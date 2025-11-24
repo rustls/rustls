df76ead2740bbe21b543a4f173cf5a175d028c5b
Joe Birr-Pixton <jpixton@gmail.com>
2025-11-24 12:58:39 +0000

    provider-example: align with workspace lints

--- provider-example/src/lib.rs
+++ provider-example/src/lib.rs
@@ -1,17 +1,4 @@
 #![no_std]
-#![warn(
-    clippy::alloc_instead_of_core,
-    clippy::manual_let_else,
-    clippy::std_instead_of_core,
-    clippy::use_self,
-    clippy::upper_case_acronyms,
-    elided_lifetimes_in_paths,
-    trivial_numeric_casts,
-    unreachable_pub,
-    unused_import_braces,
-    unused_extern_crates,
-    unused_qualifications
-)]
 
 extern crate alloc;
 #[cfg(feature = "std")]
