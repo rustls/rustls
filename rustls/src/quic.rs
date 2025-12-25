a50d3fed0477d5222ddd6cbbdccf8fc7df21e87e
Joe Birr-Pixton <jpixton@gmail.com>
2025-12-25 10:31:03 +0000

    quic: withdraw dependency on `CommonState::protocol`

--- rustls/src/quic.rs
+++ rustls/src/quic.rs
@@ -166,7 +166,7 @@ mod connection {
             let inner =
                 ConnectionCore::for_client(config, name, exts, Protocol::Quic(quic_version))?;
             Ok(Self {
-                inner: inner.into(),
+                inner: ConnectionCommon::new(inner, quic_version),
             })
         }
 
@@ -267,7 +267,8 @@ mod connection {
             };
 
             let core = ConnectionCore::for_server(config, exts, Protocol::Quic(quic_version))?;
-            Ok(Self { inner: core.into() })
+            let inner = ConnectionCommon::new(core, quic_version);
+            Ok(Self { inner })
         }
 
         /// Explicitly discard early data, notifying the client
@@ -372,9 +373,19 @@ mod connection {
         core: ConnectionCore<Side>,
         deframer_buffer: DeframerVecBuffer,
         sendable_plaintext: ChunkVecBuffer,
+        version: Version,
     }
 
     impl<Side: SideData> ConnectionCommon<Side> {
+        fn new(core: ConnectionCore<Side>, version: Version) -> Self {
+            Self {
+                core,
+                deframer_buffer: DeframerVecBuffer::default(),
+                sendable_plaintext: ChunkVecBuffer::new(Some(DEFAULT_BUFFER_LIMIT)),
+                version,
+            }
+        }
+
         /// Return the TLS-encoded transport parameters for the session's peer.
         ///
         /// While the transport parameters are technically available prior to the
@@ -401,9 +412,7 @@ mod connection {
                     SupportedCipherSuite::Tls13(suite) => Some(suite),
                     _ => None,
                 })?;
-            let Protocol::Quic(version) = self.core.common_state.protocol else {
-                return None;
-            };
+
             Some(DirectionalKeys::new(
                 suite,
                 suite.quic?,
@@ -412,7 +421,7 @@ mod connection {
                     .quic
                     .early_secret
                     .as_ref()?,
-                version,
+                self.version,
             ))
         }
 
@@ -469,16 +478,6 @@ mod connection {
     impl<Side: SideData> DerefMut for ConnectionCommon<Side> {
         fn deref_mut(&mut self) -> &mut Self::Target {
             &mut self.core.common_state
-        }
-    }
-
-    impl<Side: SideData> From<ConnectionCore<Side>> for ConnectionCommon<Side> {
-        fn from(core: ConnectionCore<Side>) -> Self {
-            Self {
-                core,
-                deframer_buffer: DeframerVecBuffer::default(),
-                sendable_plaintext: ChunkVecBuffer::new(Some(DEFAULT_BUFFER_LIMIT)),
-            }
         }
     }
 }
