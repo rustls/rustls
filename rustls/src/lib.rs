caf71e072786f3fa6770165651abb9885b51e06c
Joe Birr-Pixton <jpixton@gmail.com>
2026-01-06 15:12:40 +0000

    Move `IoState`, `wants_read` etc up from `common_state`

--- rustls/src/lib.rs
+++ rustls/src/lib.rs
@@ -437,10 +437,10 @@ pub mod unbuffered {
 
 // The public interface is:
 pub use crate::builder::{ConfigBuilder, ConfigSide, WantsVerifier};
-pub use crate::common_state::{CommonState, ConnectionOutputs, HandshakeKind, IoState};
+pub use crate::common_state::{CommonState, ConnectionOutputs, HandshakeKind};
 #[cfg(feature = "std")]
 pub use crate::conn::{Connection, Reader, Writer};
-pub use crate::conn::{ConnectionCommon, KeyingMaterialExporter, SideData, kernel};
+pub use crate::conn::{ConnectionCommon, IoState, KeyingMaterialExporter, SideData, kernel};
 pub use crate::error::Error;
 pub use crate::key_log::{KeyLog, NoKeyLog};
 pub use crate::suites::{
