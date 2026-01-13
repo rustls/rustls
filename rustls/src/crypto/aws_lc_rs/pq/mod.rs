5736a29ba5f08055e3d612abf051f47cfc97c9aa
Dirkjan Ochtman <dirkjan@ochtman.nl>
2026-01-13 17:27:44 +0100

    crypto: extract generic hybrid key exchange helper

--- rustls/src/crypto/aws_lc_rs/pq/mod.rs
+++ rustls/src/crypto/aws_lc_rs/pq/mod.rs
@@ -1,17 +1,17 @@
 use crate::crypto::aws_lc_rs::kx_group;
+use crate::crypto::kx::{Hybrid, HybridLayout};
 use crate::crypto::{NamedGroup, SupportedKxGroup};
 
-mod hybrid;
 mod mlkem;
 
 /// This is the [X25519MLKEM768] key exchange.
 ///
 /// [X25519MLKEM768]: <https://datatracker.ietf.org/doc/draft-ietf-tls-ecdhe-mlkem/>
-pub static X25519MLKEM768: &dyn SupportedKxGroup = &hybrid::Hybrid {
+pub static X25519MLKEM768: &dyn SupportedKxGroup = &Hybrid {
     classical: kx_group::X25519,
     post_quantum: MLKEM768,
     name: NamedGroup::X25519MLKEM768,
-    layout: hybrid::HybridLayout {
+    layout: HybridLayout {
         classical_share_len: X25519_LEN,
         post_quantum_client_share_len: MLKEM768_ENCAP_LEN,
         post_quantum_server_share_len: MLKEM768_CIPHERTEXT_LEN,
@@ -22,11 +22,11 @@ pub static X25519MLKEM768: &dyn SupportedKxGroup = &hybrid::Hybrid {
 /// This is the [SECP256R1MLKEM768] key exchange.
 ///
 /// [SECP256R1MLKEM768]: <https://datatracker.ietf.org/doc/draft-ietf-tls-ecdhe-mlkem/>
-pub static SECP256R1MLKEM768: &dyn SupportedKxGroup = &hybrid::Hybrid {
+pub static SECP256R1MLKEM768: &dyn SupportedKxGroup = &Hybrid {
     classical: kx_group::SECP256R1,
     post_quantum: MLKEM768,
     name: NamedGroup::secp256r1MLKEM768,
-    layout: hybrid::HybridLayout {
+    layout: HybridLayout {
         classical_share_len: SECP256R1_LEN,
         post_quantum_client_share_len: MLKEM768_ENCAP_LEN,
         post_quantum_server_share_len: MLKEM768_CIPHERTEXT_LEN,
