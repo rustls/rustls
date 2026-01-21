09c1fec463b942050af34cf7e75c17668ff770d9
Tunglies <77394545+Tunglies@users.noreply.github.com>
2026-01-22 05:02:38 +0800

    refactor: use Hashset to store ghost in LimitedCache

--- rustls/src/lib.rs
+++ rustls/src/lib.rs
@@ -461,6 +461,10 @@ pub mod time_provider;
 /// APIs abstracting over locking primitives.
 pub mod lock;
 
+mod hash_set {
+    pub(crate) use std::collections::HashSet;
+}
+
 mod hash_map {
     pub(crate) use std::collections::HashMap;
 }
