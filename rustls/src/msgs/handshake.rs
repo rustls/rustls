ed88ed46af312ddbf9852a5f9a57f47b096d1f0f
Dirkjan Ochtman <dirkjan@ochtman.nl>
2026-01-22 10:43:45 +0100

    msgs: move ECH-related server types into server_hello

--- rustls/src/msgs/handshake.rs
+++ rustls/src/msgs/handshake.rs
@@ -6,11 +6,10 @@ use core::ops::{Deref, DerefMut};
 use core::time::Duration;
 use core::{fmt, iter};
 
-use pki_types::{CertificateDer, DnsName};
+use pki_types::CertificateDer;
 
 use super::client_hello::ClientHelloPayload;
 use crate::crypto::cipher::Payload;
-use crate::crypto::hpke::{HpkeKem, HpkeSymmetricCipherSuite};
 use crate::crypto::kx::ffdhe::FfdheGroup;
 use crate::crypto::kx::{ActiveKeyExchange, KeyExchangeAlgorithm, NamedGroup};
 use crate::crypto::{
@@ -29,7 +28,7 @@ use crate::msgs::codec::{
 };
 use crate::msgs::enums::{
     CertificateStatusType, ClientCertificateType, Compression, ECCurveType, ECPointFormat,
-    EchVersion, ExtensionType, KeyUpdateRequest,
+    ExtensionType, KeyUpdateRequest,
 };
 use crate::msgs::server_hello::{ServerExtensions, ServerHelloPayload};
 use crate::sync::Arc;
@@ -152,23 +151,6 @@ impl SessionId {
 impl AsRef<[u8]> for SessionId {
     fn as_ref(&self) -> &[u8] {
         &self.data[..self.len]
-    }
-}
-
-#[derive(Clone, Debug, PartialEq)]
-pub(crate) struct UnknownExtension {
-    pub(crate) typ: ExtensionType,
-    pub(crate) payload: Payload<'static>,
-}
-
-impl UnknownExtension {
-    fn encode(&self, bytes: &mut Vec<u8>) {
-        self.payload.encode(bytes);
-    }
-
-    fn read(typ: ExtensionType, r: &mut Reader<'_>) -> Self {
-        let payload = Payload::read(r).into_owned();
-        Self { typ, payload }
     }
 }
 
@@ -1698,205 +1680,6 @@ impl<'a> HandshakeMessagePayload<'a> {
     }
 }
 
-#[derive(Clone, Debug, PartialEq)]
-pub(crate) struct HpkeKeyConfig {
-    pub config_id: u8,
-    pub kem_id: HpkeKem,
-    /// draft-ietf-tls-esni-24: `opaque HpkePublicKey<1..2^16-1>;`
-    pub public_key: SizedPayload<'static, u16, NonEmpty>,
-    pub symmetric_cipher_suites: Vec<HpkeSymmetricCipherSuite>,
-}
-
-impl Codec<'_> for HpkeKeyConfig {
-    fn encode(&self, bytes: &mut Vec<u8>) {
-        self.config_id.encode(bytes);
-        self.kem_id.encode(bytes);
-        self.public_key.encode(bytes);
-        self.symmetric_cipher_suites
-            .encode(bytes);
-    }
-
-    fn read(r: &mut Reader<'_>) -> Result<Self, InvalidMessage> {
-        Ok(Self {
-            config_id: u8::read(r)?,
-            kem_id: HpkeKem::read(r)?,
-            public_key: SizedPayload::read(r)?.into_owned(),
-            symmetric_cipher_suites: Vec::<HpkeSymmetricCipherSuite>::read(r)?,
-        })
-    }
-}
-
-#[derive(Clone, Debug, PartialEq)]
-pub(crate) struct EchConfigContents {
-    pub key_config: HpkeKeyConfig,
-    pub maximum_name_length: u8,
-    pub public_name: DnsName<'static>,
-    pub extensions: Vec<EchConfigExtension>,
-}
-
-impl EchConfigContents {
-    /// Returns true if there is more than one extension of a given
-    /// type.
-    pub(crate) fn has_duplicate_extension(&self) -> bool {
-        has_duplicates::<_, _, u16>(
-            self.extensions
-                .iter()
-                .map(|ext| ext.ext_type()),
-        )
-    }
-
-    /// Returns true if there is at least one mandatory unsupported extension.
-    pub(crate) fn has_unknown_mandatory_extension(&self) -> bool {
-        self.extensions
-            .iter()
-            // An extension is considered mandatory if the high bit of its type is set.
-            .any(|ext| {
-                matches!(ext.ext_type(), ExtensionType::Unknown(_))
-                    && u16::from(ext.ext_type()) & 0x8000 != 0
-            })
-    }
-}
-
-impl Codec<'_> for EchConfigContents {
-    fn encode(&self, bytes: &mut Vec<u8>) {
-        self.key_config.encode(bytes);
-        self.maximum_name_length.encode(bytes);
-        let dns_name = &self.public_name.borrow();
-        SizedPayload::<u8, MaybeEmpty>::from(Payload::Borrowed(dns_name.as_ref().as_ref()))
-            .encode(bytes);
-        self.extensions.encode(bytes);
-    }
-
-    fn read(r: &mut Reader<'_>) -> Result<Self, InvalidMessage> {
-        Ok(Self {
-            key_config: HpkeKeyConfig::read(r)?,
-            maximum_name_length: u8::read(r)?,
-            public_name: {
-                DnsName::try_from(SizedPayload::<u8, MaybeEmpty>::read(r)?.bytes())
-                    .map_err(|_| InvalidMessage::InvalidServerName)?
-                    .to_owned()
-            },
-            extensions: Vec::read(r)?,
-        })
-    }
-}
-
-/// An encrypted client hello (ECH) config.
-#[non_exhaustive]
-#[derive(Clone, Debug, PartialEq)]
-pub(crate) enum EchConfigPayload {
-    /// A recognized V18 ECH configuration.
-    V18(EchConfigContents),
-    /// An unknown version ECH configuration.
-    Unknown {
-        version: EchVersion,
-        contents: SizedPayload<'static, u16, MaybeEmpty>,
-    },
-}
-
-impl TlsListElement for EchConfigPayload {
-    const SIZE_LEN: ListLength = ListLength::U16;
-}
-
-impl Codec<'_> for EchConfigPayload {
-    fn encode(&self, bytes: &mut Vec<u8>) {
-        match self {
-            Self::V18(c) => {
-                // Write the version, the length, and the contents.
-                EchVersion::V18.encode(bytes);
-                let inner = LengthPrefixedBuffer::new(ListLength::U16, bytes);
-                c.encode(inner.buf);
-            }
-            Self::Unknown { version, contents } => {
-                // Unknown configuration versions are opaque.
-                version.encode(bytes);
-                contents.encode(bytes);
-            }
-        }
-    }
-
-    fn read(r: &mut Reader<'_>) -> Result<Self, InvalidMessage> {
-        let version = EchVersion::read(r)?;
-        let length = u16::read(r)?;
-        let mut contents = r.sub(length as usize)?;
-
-        Ok(match version {
-            EchVersion::V18 => Self::V18(EchConfigContents::read(&mut contents)?),
-            _ => {
-                // Note: we don't SizedPayload::read() here because we've already read the length prefix.
-                let data = SizedPayload::from(Payload::new(contents.rest()));
-                Self::Unknown {
-                    version,
-                    contents: data,
-                }
-            }
-        })
-    }
-}
-
-#[derive(Clone, Debug, PartialEq)]
-pub(crate) enum EchConfigExtension {
-    Unknown(UnknownExtension),
-}
-
-impl EchConfigExtension {
-    pub(crate) fn ext_type(&self) -> ExtensionType {
-        match self {
-            Self::Unknown(r) => r.typ,
-        }
-    }
-}
-
-impl Codec<'_> for EchConfigExtension {
-    fn encode(&self, bytes: &mut Vec<u8>) {
-        self.ext_type().encode(bytes);
-
-        let nested = LengthPrefixedBuffer::new(ListLength::U16, bytes);
-        match self {
-            Self::Unknown(r) => r.encode(nested.buf),
-        }
-    }
-
-    fn read(r: &mut Reader<'_>) -> Result<Self, InvalidMessage> {
-        let typ = ExtensionType::read(r)?;
-        let len = u16::read(r)? as usize;
-        let mut sub = r.sub(len)?;
-
-        #[expect(clippy::match_single_binding)] // Future-proofing.
-        let ext = match typ {
-            _ => Self::Unknown(UnknownExtension::read(typ, &mut sub)),
-        };
-
-        sub.expect_empty("EchConfigExtension")
-            .map(|_| ext)
-    }
-}
-
-impl TlsListElement for EchConfigExtension {
-    const SIZE_LEN: ListLength = ListLength::U16;
-}
-
-/// Representation of the ECHEncryptedExtensions extension specified in
-/// [draft-ietf-tls-esni Section 5].
-///
-/// [draft-ietf-tls-esni Section 5]: <https://www.ietf.org/archive/id/draft-ietf-tls-esni-18.html#section-5>
-#[derive(Clone, Debug)]
-pub(crate) struct ServerEncryptedClientHello {
-    pub(crate) retry_configs: Vec<EchConfigPayload>,
-}
-
-impl Codec<'_> for ServerEncryptedClientHello {
-    fn encode(&self, bytes: &mut Vec<u8>) {
-        self.retry_configs.encode(bytes);
-    }
-
-    fn read(r: &mut Reader<'_>) -> Result<Self, InvalidMessage> {
-        Ok(Self {
-            retry_configs: Vec::<EchConfigPayload>::read(r)?,
-        })
-    }
-}
-
 /// The method of encoding to use for a handshake message.
 ///
 /// In some cases a handshake message may be encoded differently depending on the purpose
@@ -1934,60 +1717,6 @@ impl DuplicateExtensionChecker {
         match self.0.insert(u) {
             true => Ok(()),
             false => Err(InvalidMessage::DuplicateExtension(u)),
-        }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::crypto::hpke::{HpkeAead, HpkeKdf};
-
-    #[test]
-    fn test_ech_config_dupe_exts() {
-        let unknown_ext = EchConfigExtension::Unknown(UnknownExtension {
-            typ: ExtensionType::Unknown(0x42),
-            payload: Payload::new(vec![0x42]),
-        });
-        let mut config = config_template();
-        config
-            .extensions
-            .push(unknown_ext.clone());
-        config.extensions.push(unknown_ext);
-
-        assert!(config.has_duplicate_extension());
-        assert!(!config.has_unknown_mandatory_extension());
-    }
-
-    #[test]
-    fn test_ech_config_mandatory_exts() {
-        let mandatory_unknown_ext = EchConfigExtension::Unknown(UnknownExtension {
-            typ: ExtensionType::Unknown(0x42 | 0x8000), // Note: high bit set.
-            payload: Payload::new(vec![0x42]),
-        });
-        let mut config = config_template();
-        config
-            .extensions
-            .push(mandatory_unknown_ext);
-
-        assert!(!config.has_duplicate_extension());
-        assert!(config.has_unknown_mandatory_extension());
-    }
-
-    fn config_template() -> EchConfigContents {
-        EchConfigContents {
-            key_config: HpkeKeyConfig {
-                config_id: 0,
-                kem_id: HpkeKem::DHKEM_P256_HKDF_SHA256,
-                public_key: SizedPayload::from(b"xxx".to_vec()),
-                symmetric_cipher_suites: vec![HpkeSymmetricCipherSuite {
-                    kdf_id: HpkeKdf::HKDF_SHA256,
-                    aead_id: HpkeAead::AES_128_GCM,
-                }],
-            },
-            maximum_name_length: 0,
-            public_name: DnsName::try_from("example.com").unwrap(),
-            extensions: vec![],
         }
     }
 }
