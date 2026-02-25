a169d0433b4511045483a79a235c79e434aaf83b
Dirkjan Ochtman <dirkjan@ochtman.nl>
2026-02-25 14:40:43 +0100

    Move TlsInputBuffer into conn

--- rustls/src/msgs/mod.rs
+++ rustls/src/msgs/mod.rs
@@ -59,7 +59,7 @@ use codec::{LengthPrefixedBuffer, U24};
 mod deframer;
 pub(crate) use deframer::{
     BufferProgress, DeframerIter, DeframerVecBuffer, Delocator, HandshakeAlignedProof,
-    HandshakeDeframer, Locator, TlsInputBuffer,
+    HandshakeDeframer, Locator,
 };
 
 mod enums;
