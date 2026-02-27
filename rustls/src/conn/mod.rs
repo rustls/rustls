4f3290f1448cb627d2d3057ad697c2cceb67633b
Dirkjan Ochtman <dirkjan@ochtman.nl>
2026-02-27 14:14:28 +0100

    conn: re-order VecBuffer methods

--- rustls/src/conn/mod.rs
+++ rustls/src/conn/mod.rs
@@ -892,34 +892,6 @@ pub struct VecBuffer {
 }
 
 impl VecBuffer {
-    /// Discard `taken` bytes from the start of our buffer.
-    pub(crate) fn discard(&mut self, taken: usize) {
-        if taken < self.used {
-            /* Before:
-             * +----------+----------+----------+
-             * | taken    | pending  |xxxxxxxxxx|
-             * +----------+----------+----------+
-             * 0          ^ taken    ^ self.used
-             *
-             * After:
-             * +----------+----------+----------+
-             * | pending  |xxxxxxxxxxxxxxxxxxxxx|
-             * +----------+----------+----------+
-             * 0          ^ self.used
-             */
-
-            self.buf
-                .copy_within(taken..self.used, 0);
-            self.used -= taken;
-        } else if taken >= self.used {
-            self.used = 0;
-        }
-    }
-
-    pub(crate) fn filled_mut(&mut self) -> &mut [u8] {
-        &mut self.buf[..self.used]
-    }
-
     /// Read some bytes from `rd`, and add them to the buffer.
     pub fn read(&mut self, rd: &mut dyn io::Read) -> io::Result<usize> {
         if let Err(err) = self.prepare_read() {
@@ -975,6 +947,30 @@ impl VecBuffer {
         Ok(())
     }
 
+    /// Discard `taken` bytes from the start of our buffer.
+    pub(crate) fn discard(&mut self, taken: usize) {
+        if taken < self.used {
+            /* Before:
+             * +----------+----------+----------+
+             * | taken    | pending  |xxxxxxxxxx|
+             * +----------+----------+----------+
+             * 0          ^ taken    ^ self.used
+             *
+             * After:
+             * +----------+----------+----------+
+             * | pending  |xxxxxxxxxxxxxxxxxxxxx|
+             * +----------+----------+----------+
+             * 0          ^ self.used
+             */
+
+            self.buf
+                .copy_within(taken..self.used, 0);
+            self.used -= taken;
+        } else if taken >= self.used {
+            self.used = 0;
+        }
+    }
+
     /// Append `bytes` to the end of this buffer.
     ///
     /// Return a `Range` saying where it went.
@@ -988,6 +984,10 @@ impl VecBuffer {
         self.buf[start..end].copy_from_slice(bytes);
         self.used += len;
         Range { start, end }
+    }
+
+    pub(crate) fn filled_mut(&mut self) -> &mut [u8] {
+        &mut self.buf[..self.used]
     }
 
     pub(crate) fn filled(&self) -> &[u8] {
