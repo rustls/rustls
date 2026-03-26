9ce094f4e221196c70dd3bd1c0366b18830c91ff
Dirkjan Ochtman <dirkjan@ochtman.nl>
2026-03-26 17:37:10 +0100

    msgs: pass whole buffer into Deframer::input_message()

--- rustls/src/conn/receive.rs
+++ rustls/src/conn/receive.rs
@@ -316,7 +316,7 @@ impl ReceivePath {
 
             let message = unborrowed.reborrow(&Delocator::new(buffer));
             self.deframer
-                .input_message(message.version, message.payload, bounds);
+                .input_message(message.version, bounds, buffer);
             self.deframer.coalesce(buffer)?;
         }
     }
