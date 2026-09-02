800d5bf47dd1f7a1e278447f5234cc297b3a76fc
Joe Birr-Pixton <jpixton@gmail.com>
2026-09-02 16:26:15 +0100

    Share `Accepted` internals between QUIC and TCP

--- rustls/src/conn/handshake.rs
+++ rustls/src/conn/handshake.rs
@@ -7,12 +7,17 @@
 //! The shims own the public signatures and documentation; the shared underlying logic lives
 //! here, parameterised by [`Transport`].
 
+use alloc::boxed::Box;
 use alloc::vec::Vec;
 
 use super::{ConnectionCommon, MessageIter, MessageIterMode, SideData};
 use crate::TlsInputBuffer;
+use crate::common_state::maybe_send_fatal_alert;
 use crate::error::Error;
+use crate::msgs::ServerExtensionsInput;
 use crate::quic::QuicOutput;
+use crate::server::{ChooseConfig, ClientHello, ServerConfig, ServerSide};
+use crate::sync::Arc;
 
 pub(crate) struct Core<Side: SideData, T: Transport> {
     pub(crate) inner: ConnectionCommon<Side>,
@@ -59,6 +64,51 @@ impl<Side: SideData, T: Transport> Core<Side, T> {
 
         result?;
         Ok(Self { inner, transport })
+    }
+}
+
+pub(crate) struct AcceptedCore<T: Transport> {
+    // invariant: `core.inner.state` is `Err(_)` and requires restoring
+    pub(crate) core: Core<ServerSide, T>,
+    choose_config: Box<ChooseConfig>,
+}
+
+impl<T: Transport> AcceptedCore<T> {
+    pub(crate) fn new(core: Core<ServerSide, T>, choose_config: Box<ChooseConfig>) -> Self {
+        Self {
+            core,
+            choose_config,
+        }
+    }
+
+    pub(crate) fn client_hello(&self) -> ClientHello<'_> {
+        self.choose_config.client_hello()
+    }
+
+    pub(crate) fn choose_config(
+        self,
+        config: Arc<ServerConfig>,
+        exts: ServerExtensionsInput,
+        tls: &mut Vec<u8>,
+    ) -> Result<Core<ServerSide, T>, Error> {
+        let Self {
+            core: Core {
+                mut inner,
+                mut transport,
+            },
+            choose_config,
+        } = self;
+
+        let result = inner.accepted(choose_config, exts, T::quic(&mut transport), config, tls);
+
+        let send_path = &mut inner.common.send;
+
+        if let Err(err) = &result {
+            maybe_send_fatal_alert(send_path, err, tls);
+        }
+
+        result?;
+        Ok(Core { inner, transport })
     }
 }
 
