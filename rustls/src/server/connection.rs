8f784a3e664bfc2219e706d394f79f4944fc0221
Joe Birr-Pixton <jpixton@gmail.com>
2026-09-02 16:24:16 +0100

    Share `NeedsInput` internals between QUIC and TCP
    This introduces `conn::handshake`, which will hold the transport-generic
    version of each handshake type, parameterised by the new `Transport`
    trait: `Tcp` writes TLS records into a caller-supplied buffer, `Quic`
    emits `QuicEvent`s instead.

--- rustls/src/server/connection.rs
+++ rustls/src/server/connection.rs
@@ -264,9 +264,7 @@ impl ServerHandshake {
     ///
     /// The returned object should be fed data from a single potential client.
     pub fn start() -> NeedsInput<ServerSide> {
-        NeedsInput {
-            inner: ConnectionCommon::for_acceptor(Protocol::Tcp),
-        }
+        NeedsInput::new(ConnectionCommon::for_acceptor(Protocol::Tcp))
     }
 }
 
@@ -296,7 +294,7 @@ impl TryFrom<ConnectionCommon<ServerSide>> for ServerHandshake {
 
             state => {
                 inner.state = Ok(state);
-                Self::NeedsInput(NeedsInput { inner })
+                Self::NeedsInput(NeedsInput::new(inner))
             }
         })
     }
@@ -347,9 +345,7 @@ impl Accepted {
 
         result?;
 
-        Ok(ServerHandshake::NeedsInput(NeedsInput {
-            inner: self.inner,
-        }))
+        Ok(ServerHandshake::NeedsInput(NeedsInput::new(self.inner)))
     }
 }
 
