cc53114799b721c7ecb925f80cef129928f51dd5
Dirkjan Ochtman <dirkjan@ochtman.nl>
2026-09-04 13:26:02 +0200

    providers: zeroize private key DER

--- rustls-ring/src/lib.rs
+++ rustls-ring/src/lib.rs
@@ -27,6 +27,7 @@ use rustls::crypto::{
 use rustls::error::Error;
 #[cfg(feature = "std")]
 use rustls::ticketer::TicketRotator;
+use zeroize::Zeroizing;
 
 /// Using software keys for authentication.
 pub mod sign;
@@ -109,16 +110,17 @@ impl KeyProvider for Ring {
         &self,
         key_der: PrivateKeyDer<'static>,
     ) -> Result<Box<dyn SigningKey>, Error> {
-        if let Ok(rsa) = RsaSigningKey::try_from(&key_der) {
+        let key_der = Zeroizing::new(key_der);
+        if let Ok(rsa) = RsaSigningKey::try_from(&*key_der) {
             return Ok(Box::new(rsa));
         }
 
-        if let Ok(ecdsa) = EcdsaSigner::try_from(&key_der) {
+        if let Ok(ecdsa) = EcdsaSigner::try_from(&*key_der) {
             return Ok(Box::new(ecdsa));
         }
 
-        if let PrivateKeyDer::Pkcs8(pkcs8) = key_der {
-            if let Ok(eddsa) = Ed25519Signer::try_from(&pkcs8) {
+        if let PrivateKeyDer::Pkcs8(pkcs8) = &*key_der {
+            if let Ok(eddsa) = Ed25519Signer::try_from(pkcs8) {
                 return Ok(Box::new(eddsa));
             }
         }
